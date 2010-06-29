e709e910462ba29c72a28cfb76ae2ffbbcf2e1d6
Makoto Inoue <makoto@new-bamboo.co.uk>
2010-06-29 12:04:20 +0100

    Changed s3 uploader to upload both full version and major_minor version

--- lib/s3_uploader.rb
+++ lib/s3_uploader.rb
@@ -11,21 +11,27 @@ module S3Uploader
 
  class << self
    def upload()
-     bucket = CONFIG[:s3][:bucket]
+     versions = [Builder.version.full, Builder.version.major_minor]
      
-     files = Dir.glob("#{Builder::DIST_DIR}/#{Builder.config["VERSION"]}/*")
-     target_dir = "#{Builder.config["VERSION"]}/"
+     versions.each do |v|
 
-     AWS::S3::Base.establish_connection!(
-       :access_key_id     => CONFIG[:s3][:access_key_id],
-       :secret_access_key => CONFIG[:s3][:secret_access_key]
-     )
+       bucket = CONFIG[:s3][:bucket]
 
-     files.each do |file|
-       file_name = File.basename(file)
-       p "Uploading ... AWS::S3::S3Object.store(#{target_dir + file_name}, open(#{file}), #{bucket}, :access => :public_read)"
-       AWS::S3::S3Object.store(target_dir + file_name, open(file), bucket, :access => :public_read)
+       files = Dir.glob("#{Builder::DIST_DIR}/#{v}/*")
+       target_dir = "#{v}/"
+
+       AWS::S3::Base.establish_connection!(
+         :access_key_id     => CONFIG[:s3][:access_key_id],
+         :secret_access_key => CONFIG[:s3][:secret_access_key]
+       )
+
+       files.each do |file|
+         file_name = File.basename(file)
+         p "Uploading ... AWS::S3::S3Object.store(#{target_dir + file_name}, open(#{file}), #{bucket}, :access => :public_read)"
+         AWS::S3::S3Object.store(target_dir + file_name, open(file), bucket, :access => :public_read)
+       end
      end
+     
    end
  end
 end
