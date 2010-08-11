ca02c519c79a0db06863eeb721f4c40e8a0927ee
Ismael Celis <ismaelct@gmail.com>
2010-08-11 16:13:59 +0100

    Send js version along with subscribe events

--- src/pusher.js
+++ src/pusher.js
@@ -139,7 +139,7 @@ Pusher.prototype = {
   
   // Not currently supported by pusherapp.com
   send_event: function(event_name, data) {
-    var payload = JSON.stringify({ 'event' : event_name, 'data' : data });
+    var payload = JSON.stringify({ 'event' : event_name, 'data' : data, 'js_version': Pusher.VERSION });
     Pusher.log("Pusher : sending event : ", payload);
     this.connection.send(payload);
     return this;
