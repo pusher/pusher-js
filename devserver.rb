2a87310e6126259d6a1ec3ebb01d8d5b6c9a6da1
Ismael Celis <ismaelct@gmail.com>
2011-01-21 17:47:15 -0300

    Dev asset server can now server both full and minified versions of every dependency. See notes.
    I need this now because min and un-min have different variable interpolations and I want to test them locally.
    
    Ie. including pusher.js will load unminified dependencies, whereas pusher.min.js loads up dependencies suffixed with ".min.js"

--- devserver.rb
+++ devserver.rb
@@ -14,22 +14,28 @@ before do
   JBundle.config_from_file 'JFile'
 end
 
-get '/dev/:version/pusher.js' do
-  content_type('application/javascript')
-  JBundle.build('pusher.js').src
-end
-
-get '/dev/:version/flashfallback.js' do
-  content_type('application/javascript')
-  JBundle.build('flashfallback.js').src
-end
-
-get '/dev/:version/json2.js' do
-  content_type('application/javascript')
-  JBundle.build('json2.js').src
+helpers do
+  def bundle(dep, mode)
+    b = JBundle.build(dep + '.js')
+    mode == nil ? b.src : b.min
+  end
 end
 
 get '/dev/:version/WebSocketMain.swf' do
   content_type("application/x-shockwave-flash")
   File.read(File.expand_path("../src/web-socket-js/WebSocketMain.swf", __FILE__))
+end
+
+# == Catch all for:
+# /dev/:version/pusher.js
+# /dev/:version/pusher.min.js
+# /dev/:version/json2.js
+# /dev/:version/json2.min.js
+# /dev/:version/flashfallback.js
+# /dev/:version/flashfallback.min.js
+# Note: :version is not used but it's easier to have it so it's consistent with production asset URLs
+#
+get %r{/dev/([\w\d\.]+)/([\d\w]+)(\.min)?} do |version, dep, mode|
+  content_type('application/javascript')
+  bundle dep, mode
 end
