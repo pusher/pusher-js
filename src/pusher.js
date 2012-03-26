aef4525c4c24c63b1447d3554c162cdedaec9ebd
Mary Rose Cook <maryrosecook@maryrosecook.com>
2012-03-26 16:42:44 +0100

    Put pusher.js and pusher_channels.js in closures.

--- src/pusher.js
+++ src/pusher.js
@@ -1,211 +1,213 @@
-if (Function.prototype.scopedTo === undefined) {
-  Function.prototype.scopedTo = function(context, args) {
-    var f = this;
-    return function() {
-      return f.apply(context, Array.prototype.slice.call(args || [])
-        .concat(Array.prototype.slice.call(arguments)));
+;(function() {
+  if (Function.prototype.scopedTo === undefined) {
+    Function.prototype.scopedTo = function(context, args) {
+      var f = this;
+      return function() {
+        return f.apply(context, Array.prototype.slice.call(args || [])
+                       .concat(Array.prototype.slice.call(arguments)));
+      };
     };
-  };
-}
+  }
 
-var Pusher = function(app_key, options) {
-  this.options = options || {};
-  this.key = app_key;
-  this.channels = new Pusher.Channels();
-  this.global_emitter = new Pusher.EventsDispatcher()
+  var Pusher = function(app_key, options) {
+    this.options = options || {};
+    this.key = app_key;
+    this.channels = new Pusher.Channels();
+    this.global_emitter = new Pusher.EventsDispatcher()
 
-  var self = this;
-
-  this.checkAppKey();
-
-  this.connection = new Pusher.Connection(this.key, this.options);
-
-  // Setup / teardown connection
-  this.connection
-    .bind('connected', function() {
-      self.subscribeAll();
-    })
-    .bind('message', function(params) {
-      var internal = (params.event.indexOf('pusher_internal:') === 0);
-      if (params.channel) {
-        var channel;
-        if (channel = self.channel(params.channel)) {
-          channel.emit(params.event, params.data);
-        }
-      }
-      // Emit globaly [deprecated]
-      if (!internal) self.global_emitter.emit(params.event, params.data);
-    })
-    .bind('disconnected', function() {
-      self.channels.disconnect();
-    })
-    .bind('error', function(err) {
-      Pusher.warn('Error', err);
-    });
-
-  Pusher.instances.push(this);
-
-  if (Pusher.isReady) self.connect();
-};
-Pusher.instances = [];
-Pusher.prototype = {
-  channel: function(name) {
-    return this.channels.find(name);
-  },
-
-  connect: function() {
-    this.connection.connect();
-  },
-
-  disconnect: function() {
-    this.connection.disconnect();
-  },
-
-  bind: function(event_name, callback) {
-    this.global_emitter.bind(event_name, callback);
-    return this;
-  },
-
-  bind_all: function(callback) {
-    this.global_emitter.bind_all(callback);
-    return this;
-  },
-
-  subscribeAll: function() {
-    var channel;
-    for (channelName in this.channels.channels) {
-      if (this.channels.channels.hasOwnProperty(channelName)) {
-        this.subscribe(channelName, this.channels.channels[channelName].subscribeOptions);
-      }
-    }
-  },
-
-  subscribe: function(channel_name, subscribeOptions) {
     var self = this;
-    var channel = this.channels.add(channel_name, this);
 
-    channel.subscribeOptions = subscribeOptions; // save options used to subscribe for reconnects
+    this.checkAppKey();
 
-    var options = Pusher.composeOptions(this.options, subscribeOptions);
-    if (this.connection.state === 'connected') {
-      channel.authorize(this.connection.socket_id, options, function(err, data) {
-        if (err) {
-          channel.emit('pusher:subscription_error', data);
-        } else {
-          self.send_event('pusher:subscribe', {
-            channel: channel_name,
-            auth: data.auth,
-            channel_data: data.channel_data
-          });
+    this.connection = new Pusher.Connection(this.key, this.options);
+
+    // Setup / teardown connection
+    this.connection
+      .bind('connected', function() {
+        self.subscribeAll();
+      })
+      .bind('message', function(params) {
+        var internal = (params.event.indexOf('pusher_internal:') === 0);
+        if (params.channel) {
+          var channel;
+          if (channel = self.channel(params.channel)) {
+            channel.emit(params.event, params.data);
+          }
         }
+        // Emit globaly [deprecated]
+        if (!internal) self.global_emitter.emit(params.event, params.data);
+      })
+      .bind('disconnected', function() {
+        self.channels.disconnect();
+      })
+      .bind('error', function(err) {
+        Pusher.warn('Error', err);
       });
-    }
-    return channel;
-  },
 
-  unsubscribe: function(channel_name) {
-    this.channels.remove(channel_name);
-    if (this.connection.state === 'connected') {
-      this.send_event('pusher:unsubscribe', {
-        channel: channel_name
-      });
-    }
-  },
+    Pusher.instances.push(this);
 
-  send_event: function(event_name, data, channel) {
-    return this.connection.send_event(event_name, data, channel);
-  },
+    if (Pusher.isReady) self.connect();
+  };
+  Pusher.instances = [];
+  Pusher.prototype = {
+    channel: function(name) {
+      return this.channels.find(name);
+    },
 
-  checkAppKey: function() {
-    if(this.key === null || this.key === undefined) {
-      Pusher.warn('Warning', 'You must pass your app key when you instantiate Pusher.');
-    }
-  }
-};
+    connect: function() {
+      this.connection.connect();
+    },
 
-Pusher.Util = {
-  extend: function extend(target, extensions) {
-    for (var property in extensions) {
-      if (extensions[property] && extensions[property].constructor &&
-        extensions[property].constructor === Object) {
-        target[property] = extend(target[property] || {}, extensions[property]);
-      } else {
-        target[property] = extensions[property];
-      }
-    }
-    return target;
-  },
+    disconnect: function() {
+      this.connection.disconnect();
+    },
 
-  stringify: function stringify() {
-    var m = ["Pusher"]
-    for (var i = 0; i < arguments.length; i++){
-      if (typeof arguments[i] === "string") {
-        m.push(arguments[i])
-      } else {
-        if (window['JSON'] == undefined) {
-          m.push(arguments[i].toString());
-        } else {
-          m.push(JSON.stringify(arguments[i]))
+    bind: function(event_name, callback) {
+      this.global_emitter.bind(event_name, callback);
+      return this;
+    },
+
+    bind_all: function(callback) {
+      this.global_emitter.bind_all(callback);
+      return this;
+    },
+
+    subscribeAll: function() {
+      var channel;
+      for (channelName in this.channels.channels) {
+        if (this.channels.channels.hasOwnProperty(channelName)) {
+          this.subscribe(channelName, this.channels.channels[channelName].subscribeOptions);
         }
       }
-    };
-    return m.join(" : ")
-  },
+    },
 
-  arrayIndexOf: function(array, item) { // MSIE doesn't have array.indexOf
-    var nativeIndexOf = Array.prototype.indexOf;
-    if (array == null) return -1;
-    if (nativeIndexOf && array.indexOf === nativeIndexOf) return array.indexOf(item);
-    for (i = 0, l = array.length; i < l; i++) if (array[i] === item) return i;
-    return -1;
-  }
-};
+    subscribe: function(channel_name, subscribeOptions) {
+      var self = this;
+      var channel = this.channels.add(channel_name, this);
 
-// To receive log output provide a Pusher.log function, for example
-// Pusher.log = function(m){console.log(m)}
-Pusher.debug = function() {
-  if (!Pusher.log) return
-  Pusher.log(Pusher.Util.stringify.apply(this, arguments))
-}
-Pusher.warn = function() {
-  if (window.console && window.console.warn) {
-    window.console.warn(Pusher.Util.stringify.apply(this, arguments));
-  } else {
+      channel.subscribeOptions = subscribeOptions; // save options used to subscribe for reconnects
+
+      var options = Pusher.composeOptions(this.options, subscribeOptions);
+      if (this.connection.state === 'connected') {
+        channel.authorize(this.connection.socket_id, options, function(err, data) {
+          if (err) {
+            channel.emit('pusher:subscription_error', data);
+          } else {
+            self.send_event('pusher:subscribe', {
+              channel: channel_name,
+              auth: data.auth,
+              channel_data: data.channel_data
+            });
+          }
+        });
+      }
+      return channel;
+    },
+
+    unsubscribe: function(channel_name) {
+      this.channels.remove(channel_name);
+      if (this.connection.state === 'connected') {
+        this.send_event('pusher:unsubscribe', {
+          channel: channel_name
+        });
+      }
+    },
+
+    send_event: function(event_name, data, channel) {
+      return this.connection.send_event(event_name, data, channel);
+    },
+
+    checkAppKey: function() {
+      if(this.key === null || this.key === undefined) {
+        Pusher.warn('Warning', 'You must pass your app key when you instantiate Pusher.');
+      }
+    }
+  };
+
+  Pusher.Util = {
+    extend: function extend(target, extensions) {
+      for (var property in extensions) {
+        if (extensions[property] && extensions[property].constructor &&
+            extensions[property].constructor === Object) {
+          target[property] = extend(target[property] || {}, extensions[property]);
+        } else {
+          target[property] = extensions[property];
+        }
+      }
+      return target;
+    },
+
+    stringify: function stringify() {
+      var m = ["Pusher"]
+      for (var i = 0; i < arguments.length; i++){
+        if (typeof arguments[i] === "string") {
+          m.push(arguments[i])
+        } else {
+          if (window['JSON'] == undefined) {
+            m.push(arguments[i].toString());
+          } else {
+            m.push(JSON.stringify(arguments[i]))
+          }
+        }
+      };
+      return m.join(" : ")
+    },
+
+    arrayIndexOf: function(array, item) { // MSIE doesn't have array.indexOf
+      var nativeIndexOf = Array.prototype.indexOf;
+      if (array == null) return -1;
+      if (nativeIndexOf && array.indexOf === nativeIndexOf) return array.indexOf(item);
+      for (i = 0, l = array.length; i < l; i++) if (array[i] === item) return i;
+      return -1;
+    }
+  };
+
+  // To receive log output provide a Pusher.log function, for example
+  // Pusher.log = function(m){console.log(m)}
+  Pusher.debug = function() {
     if (!Pusher.log) return
-    Pusher.log(Pusher.Util.stringify.apply(this, arguments));
+    Pusher.log(Pusher.Util.stringify.apply(this, arguments))
   }
-};
+  Pusher.warn = function() {
+    if (window.console && window.console.warn) {
+      window.console.warn(Pusher.Util.stringify.apply(this, arguments));
+    } else {
+      if (!Pusher.log) return
+      Pusher.log(Pusher.Util.stringify.apply(this, arguments));
+    }
+  };
 
-// Pusher defaults
-Pusher.VERSION = '<VERSION>';
+  // Pusher defaults
+  Pusher.VERSION = '<VERSION>';
 
-Pusher.host = 'ws.pusherapp.com';
-Pusher.ws_port = 80;
-Pusher.wss_port = 443;
-Pusher.channel_auth_endpoint = '/pusher/auth';
-Pusher.cdn_http = '<CDN_HTTP>'
-Pusher.cdn_https = '<CDN_HTTPS>'
-Pusher.dependency_suffix = '<DEPENDENCY_SUFFIX>';
-Pusher.channel_auth_transport = 'ajax';
-Pusher.activity_timeout = 120000;
-Pusher.pong_timeout = 30000;
+  Pusher.host = 'ws.pusherapp.com';
+  Pusher.ws_port = 80;
+  Pusher.wss_port = 443;
+  Pusher.channel_auth_endpoint = '/pusher/auth';
+  Pusher.cdn_http = '<CDN_HTTP>'
+  Pusher.cdn_https = '<CDN_HTTPS>'
+  Pusher.dependency_suffix = '<DEPENDENCY_SUFFIX>';
+  Pusher.channel_auth_transport = 'ajax';
+  Pusher.activity_timeout = 120000;
+  Pusher.pong_timeout = 30000;
 
-Pusher.isReady = false;
-Pusher.ready = function() {
-  Pusher.isReady = true;
-  for (var i = 0, l = Pusher.instances.length; i < l; i++) {
-    Pusher.instances[i].connect();
-  }
-};
+  Pusher.isReady = false;
+  Pusher.ready = function() {
+    Pusher.isReady = true;
+    for (var i = 0, l = Pusher.instances.length; i < l; i++) {
+      Pusher.instances[i].connect();
+    }
+  };
 
-Pusher.composeOptions = function(globalOptions, subscribeOptions) {
-  globalOptions = globalOptions || {};
-  subscribeOptions = subscribeOptions || {};
+  Pusher.composeOptions = function(globalOptions, subscribeOptions) {
+    globalOptions = globalOptions || {};
+    subscribeOptions = subscribeOptions || {};
 
-  var options = {};
-  Pusher.Util.extend(options, globalOptions);
-  Pusher.Util.extend(options, subscribeOptions); // any clashes result in sub call options overriding global ones
-  return options;
-};
+    var options = {};
+    Pusher.Util.extend(options, globalOptions);
+    Pusher.Util.extend(options, subscribeOptions); // any clashes result in sub call options overriding global ones
+    return options;
+  };
 
-this.Pusher = Pusher;
+  this.Pusher = Pusher;
+}).call(this);
--- src/pusher_channels.js
+++ src/pusher_channels.js
@@ -1,158 +1,160 @@
-Pusher.Channels = function() {
-  this.channels = {};
-};
-
-Pusher.Channels.prototype = {
-  add: function(channel_name, pusher) {
-    var existing_channel = this.find(channel_name);
-    if (!existing_channel) {
-      var channel = Pusher.Channel.factory(channel_name, pusher);
-      this.channels[channel_name] = channel;
-      return channel;
-    } else {
-      return existing_channel;
-    }
-  },
-
-  find: function(channel_name) {
-    return this.channels[channel_name];
-  },
-
-  remove: function(channel_name) {
-    delete this.channels[channel_name];
-  },
-
-  disconnect: function () {
-    for(var channel_name in this.channels){
-      this.channels[channel_name].disconnect()
-    }
-  }
-};
-
-Pusher.Channel = function(channel_name, pusher) {
-  var self = this;
-  Pusher.EventsDispatcher.call(this, function(event_name, event_data) {
-    Pusher.debug('No callbacks on ' + channel_name + ' for ' + event_name);
-  });
-
-  this.pusher = pusher;
-  this.name = channel_name;
-  this.subscribed = false;
-
-  this.bind('pusher_internal:subscription_succeeded', function(data) {
-    self.onSubscriptionSucceeded(data);
-  });
-};
-
-Pusher.Channel.prototype = {
-  // inheritable constructor
-  init: function() {},
-  disconnect: function() {},
-
-  onSubscriptionSucceeded: function(data) {
-    this.subscribed = true;
-    this.emit('pusher:subscription_succeeded');
-  },
-
-  authorize: function(socketId, options, callback){
-    return callback(false, {}); // normal channels don't require auth
-  },
-
-  trigger: function(event, data) {
-    return this.pusher.send_event(event, data, this.name);
-  }
-};
-
-Pusher.Util.extend(Pusher.Channel.prototype, Pusher.EventsDispatcher.prototype);
-
-Pusher.Channel.PrivateChannel = {
-  authorize: function(socketId, options, callback){
-    var authorizer = new Pusher.Channel.Authorizer(this, Pusher.channel_auth_transport, options);
-    return authorizer.authorize(socketId, callback);
-  }
-};
-
-Pusher.Channel.PresenceChannel = {
-  init: function(){
-    this.bind('pusher_internal:member_added', function(data){
-      var member = this.members.add(data.user_id, data.user_info);
-      this.emit('pusher:member_added', member);
-    }.scopedTo(this))
-
-    this.bind('pusher_internal:member_removed', function(data){
-      var member = this.members.remove(data.user_id);
-      if (member) {
-        this.emit('pusher:member_removed', member);
-      }
-    }.scopedTo(this))
-  },
-
-  disconnect: function(){
-    this.members.clear();
-  },
-
-  onSubscriptionSucceeded: function(data) {
-    this.members._members_map = data.presence.hash;
-    this.members.count = data.presence.count;
-    this.subscribed = true;
-
-    this.emit('pusher:subscription_succeeded', this.members);
-  },
-
-  members: {
-    _members_map: {},
-    count: 0,
-
-    each: function(callback) {
-      for(var i in this._members_map) {
-        callback({
-          id: i,
-          info: this._members_map[i]
-        });
-      }
-    },
-
-    add: function(id, info) {
-      this._members_map[id] = info;
-      this.count++;
-      return this.get(id);
-    },
-
-    remove: function(user_id) {
-      var member = this.get(user_id);
-      if (member) {
-        delete this._members_map[user_id];
-        this.count--;
-      }
-      return member;
-    },
-
-    get: function(user_id) {
-      if (this._members_map.hasOwnProperty(user_id)) { // have heard of this user user_id
-        return {
-          id: user_id,
-          info: this._members_map[user_id]
-        }
-      } else { // have never heard of this user
-        return null;
-      }
-    },
-
-    clear: function() {
-      this._members_map = {};
-      this.count = 0;
-    }
-  }
-};
-
-Pusher.Channel.factory = function(channel_name, pusher){
-  var channel = new Pusher.Channel(channel_name, pusher);
-  if (channel_name.indexOf('private-') === 0) {
-    Pusher.Util.extend(channel, Pusher.Channel.PrivateChannel);
-  } else if (channel_name.indexOf('presence-') === 0) {
-    Pusher.Util.extend(channel, Pusher.Channel.PrivateChannel);
-    Pusher.Util.extend(channel, Pusher.Channel.PresenceChannel);
+;(function() {
+  Pusher.Channels = function() {
+    this.channels = {};
   };
-  channel.init();
-  return channel;
-};
+
+  Pusher.Channels.prototype = {
+    add: function(channel_name, pusher) {
+      var existing_channel = this.find(channel_name);
+      if (!existing_channel) {
+        var channel = Pusher.Channel.factory(channel_name, pusher);
+        this.channels[channel_name] = channel;
+        return channel;
+      } else {
+        return existing_channel;
+      }
+    },
+
+    find: function(channel_name) {
+      return this.channels[channel_name];
+    },
+
+    remove: function(channel_name) {
+      delete this.channels[channel_name];
+    },
+
+    disconnect: function () {
+      for(var channel_name in this.channels){
+        this.channels[channel_name].disconnect()
+      }
+    }
+  };
+
+  Pusher.Channel = function(channel_name, pusher) {
+    var self = this;
+    Pusher.EventsDispatcher.call(this, function(event_name, event_data) {
+      Pusher.debug('No callbacks on ' + channel_name + ' for ' + event_name);
+    });
+
+    this.pusher = pusher;
+    this.name = channel_name;
+    this.subscribed = false;
+
+    this.bind('pusher_internal:subscription_succeeded', function(data) {
+      self.onSubscriptionSucceeded(data);
+    });
+  };
+
+  Pusher.Channel.prototype = {
+    // inheritable constructor
+    init: function() {},
+    disconnect: function() {},
+
+    onSubscriptionSucceeded: function(data) {
+      this.subscribed = true;
+      this.emit('pusher:subscription_succeeded');
+    },
+
+    authorize: function(socketId, options, callback){
+      return callback(false, {}); // normal channels don't require auth
+    },
+
+    trigger: function(event, data) {
+      return this.pusher.send_event(event, data, this.name);
+    }
+  };
+
+  Pusher.Util.extend(Pusher.Channel.prototype, Pusher.EventsDispatcher.prototype);
+
+  Pusher.Channel.PrivateChannel = {
+    authorize: function(socketId, options, callback){
+      var authorizer = new Pusher.Channel.Authorizer(this, Pusher.channel_auth_transport, options);
+      return authorizer.authorize(socketId, callback);
+    }
+  };
+
+  Pusher.Channel.PresenceChannel = {
+    init: function(){
+      this.bind('pusher_internal:member_added', function(data){
+        var member = this.members.add(data.user_id, data.user_info);
+        this.emit('pusher:member_added', member);
+      }.scopedTo(this))
+
+      this.bind('pusher_internal:member_removed', function(data){
+        var member = this.members.remove(data.user_id);
+        if (member) {
+          this.emit('pusher:member_removed', member);
+        }
+      }.scopedTo(this))
+    },
+
+    disconnect: function(){
+      this.members.clear();
+    },
+
+    onSubscriptionSucceeded: function(data) {
+      this.members._members_map = data.presence.hash;
+      this.members.count = data.presence.count;
+      this.subscribed = true;
+
+      this.emit('pusher:subscription_succeeded', this.members);
+    },
+
+    members: {
+      _members_map: {},
+      count: 0,
+
+      each: function(callback) {
+        for(var i in this._members_map) {
+          callback({
+            id: i,
+            info: this._members_map[i]
+          });
+        }
+      },
+
+      add: function(id, info) {
+        this._members_map[id] = info;
+        this.count++;
+        return this.get(id);
+      },
+
+      remove: function(user_id) {
+        var member = this.get(user_id);
+        if (member) {
+          delete this._members_map[user_id];
+          this.count--;
+        }
+        return member;
+      },
+
+      get: function(user_id) {
+        if (this._members_map.hasOwnProperty(user_id)) { // have heard of this user user_id
+          return {
+            id: user_id,
+            info: this._members_map[user_id]
+          }
+        } else { // have never heard of this user
+          return null;
+        }
+      },
+
+      clear: function() {
+        this._members_map = {};
+        this.count = 0;
+      }
+    }
+  };
+
+  Pusher.Channel.factory = function(channel_name, pusher){
+    var channel = new Pusher.Channel(channel_name, pusher);
+    if (channel_name.indexOf('private-') === 0) {
+      Pusher.Util.extend(channel, Pusher.Channel.PrivateChannel);
+    } else if (channel_name.indexOf('presence-') === 0) {
+      Pusher.Util.extend(channel, Pusher.Channel.PrivateChannel);
+      Pusher.Util.extend(channel, Pusher.Channel.PresenceChannel);
+    };
+    channel.init();
+    return channel;
+  };
+}).call(this);
