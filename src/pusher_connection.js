d92700dfc9afd0c22fcbca3cfc61412a063d95d6
Mary Rose Cook <maryrosecook@maryrosecook.com>
2012-05-16 16:27:29 +0100

    Prevent failed -> failed on connect()
    * Connection already failed, then .connect() is called.  Fix prevents second transition to failed.
    * Regression test.

--- src/pusher_connection.js
+++ src/pusher_connection.js
@@ -426,7 +426,8 @@
 
   Connection.prototype.connect = function() {
     // no WebSockets
-    if (Pusher.Transport === null || Pusher.Transport === undefined) {
+    if (!this._machine.is('failed')
+        && (Pusher.Transport === null || Pusher.Transport === undefined)) {
       this._machine.transition('failed');
     }
     // initial open of connection
