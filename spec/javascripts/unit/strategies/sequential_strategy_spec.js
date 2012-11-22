04a0048b183c88e0ecfd47dcbccf1d48d03de89d
Paweł Ledwoń <me@ludvik.pl>
2012-11-22 14:30:59 +0000

    Implement sequential strategy
    * support timeouts
    * support looping
    * allow aborting
    * include specs

---
+++ spec/javascripts/unit/strategies/sequential_strategy_spec.js
@@ -0,0 +1,257 @@
+describe("SequentialStrategy", function() {
+  function getSubstrategyMock(supported) {
+    var substrategy = new Pusher.EventsDispatcher();
+
+    substrategy.initialize = jasmine.createSpy("initialize");
+    substrategy.connect = jasmine.createSpy("connect");
+    substrategy.abort = jasmine.createSpy("abort");
+    substrategy.isSupported = jasmine.createSpy("initialize")
+      .andReturn(supported);
+
+    return substrategy;
+  }
+
+  it("should expose its name", function() {
+    expect(new Pusher.SequentialStrategy([]).name).toEqual("seq");
+  });
+
+  describe("when asked if it's supported", function() {
+    it("should return true when one of substrategies is supported", function() {
+      var substrategies = [
+        getSubstrategyMock(false),
+        getSubstrategyMock(true),
+      ];
+      var strategy = new Pusher.SequentialStrategy(substrategies);
+
+      expect(strategy.isSupported()).toBe(true);
+    });
+
+    it("should return false when none of substrategies is supported", function() {
+      var substrategies = [
+        getSubstrategyMock(false),
+        getSubstrategyMock(false),
+      ];
+      var strategy = new Pusher.SequentialStrategy(substrategies);
+
+      expect(strategy.isSupported()).toBe(false);
+    });
+  });
+
+  describe("on initialization", function() {
+    it("should delegate initialization to all substrategies immediately", function() {
+      var substrategies = [
+        getSubstrategyMock(true),
+        getSubstrategyMock(false),
+        getSubstrategyMock(true),
+      ];
+      var strategy = new Pusher.SequentialStrategy(substrategies);
+
+      strategy.initialize();
+
+      expect(substrategies[0].initialize).toHaveBeenCalled();
+      expect(substrategies[1].initialize).not.toHaveBeenCalled();
+      expect(substrategies[2].initialize).toHaveBeenCalled();
+    });
+  });
+
+  describe("on connection attempt", function() {
+    it("should finish on first successful substrategy", function() {
+      var substrategies = [
+        getSubstrategyMock(true),
+        getSubstrategyMock(true),
+      ];
+      var strategy = new Pusher.SequentialStrategy(substrategies);
+
+      var openCallback = jasmine.createSpy("openCallback");
+      strategy.bind("open", openCallback);
+
+      strategy.connect();
+
+      expect(substrategies[0].connect).toHaveBeenCalled();
+      expect(substrategies[0].abort).not.toHaveBeenCalled();
+      expect(substrategies[1].connect).not.toHaveBeenCalled();
+
+      var connection = {};
+      substrategies[0].emit("open", connection);
+
+      expect(openCallback).toHaveBeenCalledWith(connection);
+      expect(substrategies[0].abort).not.toHaveBeenCalled();
+      expect(substrategies[1].connect).not.toHaveBeenCalled();
+    });
+
+    it("should fail after trying all supported substrategies", function() {
+      var substrategies = [
+        getSubstrategyMock(true),
+        getSubstrategyMock(false),
+        getSubstrategyMock(true),
+      ];
+      var strategy = new Pusher.SequentialStrategy(substrategies);
+
+      var errorCallback = jasmine.createSpy("errorCallback");
+      strategy.bind("error", errorCallback);
+
+      strategy.connect();
+
+      expect(substrategies[0].connect).toHaveBeenCalled();
+      expect(substrategies[1].connect).not.toHaveBeenCalled();
+
+      substrategies[0].emit("error", 1);
+
+      expect(substrategies[0].abort).not.toHaveBeenCalled();
+      expect(substrategies[1].connect).not.toHaveBeenCalled();
+      expect(substrategies[1].abort).not.toHaveBeenCalled();
+      expect(substrategies[2].connect).toHaveBeenCalled();
+
+      expect(errorCallback).not.toHaveBeenCalled();
+
+      substrategies[2].emit("error", 2);
+
+      expect(substrategies[2].abort).not.toHaveBeenCalled();
+      expect(errorCallback).toHaveBeenCalled();
+    });
+
+    it("should support looping", function() {
+      var substrategies = [
+        getSubstrategyMock(true),
+        getSubstrategyMock(true),
+      ];
+      var strategy = new Pusher.SequentialStrategy(substrategies, {
+        loop: true,
+      });
+
+      strategy.connect();
+
+      expect(substrategies[0].connect.calls.length).toEqual(1);
+      expect(substrategies[1].connect.calls.length).toEqual(0);
+
+      substrategies[0].emit("error", 1);
+
+      expect(substrategies[0].connect.calls.length).toEqual(1);
+      expect(substrategies[1].connect.calls.length).toEqual(1);
+
+      substrategies[1].emit("error", 2);
+
+      expect(substrategies[0].connect.calls.length).toEqual(2);
+      expect(substrategies[1].connect.calls.length).toEqual(1);
+    });
+
+    it("should not allow second attempt", function() {
+      var substrategies = [
+        getSubstrategyMock(true),
+      ];
+      var strategy = new Pusher.SequentialStrategy(substrategies);
+
+      expect(strategy.connect()).toBe(true);
+      expect(substrategies[0].connect.calls.length).toEqual(1);
+
+      expect(strategy.connect()).toBe(false);
+      expect(substrategies[0].connect.calls.length).toEqual(1);
+    });
+  });
+
+  describe("on aborting", function() {
+    it("should send abort to substrategy and not try another one", function() {
+      var substrategies = [
+        getSubstrategyMock(true),
+        getSubstrategyMock(true),
+      ];
+      var strategy = new Pusher.SequentialStrategy(substrategies, {});
+
+      strategy.connect();
+
+      expect(substrategies[0].connect).toHaveBeenCalled();
+      expect(substrategies[1].connect).not.toHaveBeenCalled();
+
+      strategy.abort();
+
+      expect(substrategies[0].abort).toHaveBeenCalled();
+      expect(substrategies[1].connect).not.toHaveBeenCalled();
+    });
+
+    it("should not abort when there is no attempt being made", function() {
+      var substrategies = [
+        getSubstrategyMock(true),
+      ];
+      var strategy = new Pusher.SequentialStrategy(substrategies);
+
+      expect(strategy.abort()).toBe(false);
+      expect(substrategies[0].connect.calls.length).toEqual(0);
+      expect(substrategies[0].abort.calls.length).toEqual(0);
+    });
+  });
+
+  describe("on timeout", function() {
+    it("should advance to next substrategy if possible", function() {
+      var substrategies = [
+        getSubstrategyMock(true),
+        getSubstrategyMock(true),
+      ];
+      var strategy = new Pusher.SequentialStrategy(substrategies, { timeout: 100 });
+
+      var testStartTimestamp = Date.now();
+      var nextStrategyDelay = null;
+
+      runs(function() {
+        strategy.connect();
+      });
+      waitsFor(function() {
+        nextStrategyDelay = Date.now() - testStartTimestamp;
+        return substrategies[1].connect.wasCalled;
+      }, "error callback to be called", 190);
+      runs(function() {
+        expect(nextStrategyDelay).toBeGreaterThan(99);
+      });
+    });
+
+    it("should increase timeout exponentially until the limit", function() {
+      var substrategies = [
+        getSubstrategyMock(true),
+        getSubstrategyMock(true),
+        getSubstrategyMock(true),
+        getSubstrategyMock(true),
+        getSubstrategyMock(true),
+      ];
+      var strategy = new Pusher.SequentialStrategy(substrategies, {
+        timeout: 100,
+        timeoutLimit: 400,
+      });
+
+      strategy.connect();
+
+      var substrategyStartTimestamp = null;
+      var substrategyDelay = null;
+
+      runs(function() {
+        substrategyStartTimestamp = Date.now();
+      });
+      waitsFor(function() {
+        substrategyDelay = Date.now() - substrategyStartTimestamp;
+        return substrategies[1].connect.wasCalled;
+      }, "error callback to be called", 190);
+      runs(function() {
+        expect(substrategyDelay).toBeGreaterThan(99);
+      });
+      waitsFor(function() {
+        substrategyDelay = Date.now() - substrategyStartTimestamp;
+        return substrategies[2].connect.wasCalled;
+      }, "error callback to be called", 290);
+      runs(function() {
+        expect(substrategyDelay).toBeGreaterThan(199);
+      });
+      waitsFor(function() {
+        substrategyDelay = Date.now() - substrategyStartTimestamp;
+        return substrategies[3].connect.wasCalled;
+      }, "error callback to be called", 490);
+      runs(function() {
+        expect(substrategyDelay).toBeGreaterThan(399);
+      });
+      waitsFor(function() {
+        substrategyDelay = Date.now() - substrategyStartTimestamp;
+        return substrategies[4].connect.wasCalled;
+      }, "error callback to be called", 490);
+      runs(function() {
+        expect(substrategyDelay).toBeGreaterThan(399);
+      });
+    });
+  });
+});
---
+++ src/strategies/sequential_strategy.js
@@ -0,0 +1,135 @@
+;(function() {
+
+  function SequentialStrategy(substrategies, options) {
+    Pusher.EventsDispatcher.call(this);
+
+    this.substrategies = substrategies;
+    this.options = options || {};
+  };
+  var prototype = SequentialStrategy.prototype;
+
+  Pusher.Util.extend(prototype, Pusher.EventsDispatcher.prototype);
+
+  // interface
+
+  prototype.name = "seq";
+
+  prototype.isSupported = function() {
+    return this.getSupported().length > 0;
+  };
+
+  prototype.initialize = function() {
+    var strategies = this.getSupported();
+
+    for (var i = 0; i < strategies.length; i++) {
+      strategies[i].initialize();
+    }
+  };
+
+  prototype.connect = function() {
+    if (this.abortCallback) {
+      return false;
+    }
+
+    var self = this;
+
+    var strategies = this.getSupported();
+    var current = 0;
+    var timeout = this.options.timeout;
+
+    var tryNextStrategy = function(error, connection) {
+      if (connection) {
+        self.emit("open", connection);
+      } else {
+        current = current + 1;
+        if (self.options.loop) {
+          current = current % strategies.length;
+        }
+
+        if (current < strategies.length) {
+          if (timeout) {
+            timeout = timeout * 2;
+            if (self.options.timeoutLimit) {
+              timeout = Math.min(timeout, self.options.timeoutLimit);
+            }
+          }
+          this.abortCallback = self.tryStrategy(
+            strategies[current], timeout, tryNextStrategy
+          );
+        } else {
+          self.emit("error");
+        }
+      }
+    };
+
+    this.abortCallback = this.tryStrategy(
+      strategies[current], this.options.timeout, tryNextStrategy
+    );
+
+    return true;
+  };
+
+  prototype.abort = function() {
+    if (this.abortCallback) {
+      this.abortCallback();
+      this.abortCallback = null;
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  // private
+
+  prototype.getSupported = function() {
+    var supported = [];
+    for (var i = 0; i < this.substrategies.length; i++) {
+      if (this.substrategies[i].isSupported()) {
+        supported.push(this.substrategies[i]);
+      }
+    }
+    return supported;
+  }
+
+  prototype.tryStrategy = function(strategy, timeoutLength, callback) {
+    var onOpen = function(connection) {
+      unbindListeners();
+      callback(null, connection);
+    };
+    var onError = function(error) {
+      unbindListeners();
+      callback(error);
+    };
+    var onTimeout = function() {
+      strategy.abort();
+      unbindListeners();
+      callback("timeout");
+    };
+
+    if (timeoutLength > 0) {
+      var timeout = setTimeout(onTimeout, timeoutLength);
+    }
+
+    var unbindListeners = function() {
+      strategy.unbind("open", onOpen);
+      strategy.unbind("error", onError);
+      if (timeout) {
+        clearTimeout(timeout);
+      }
+    };
+
+    var abortCallback = function() {
+      strategy.abort();
+      unbindListeners();
+    };
+
+    strategy.bind("open", onOpen);
+    strategy.bind("error", onError);
+
+    strategy.connect();
+
+    return abortCallback;
+  };
+
+  Pusher.SequentialStrategy = SequentialStrategy;
+}).call(this);
