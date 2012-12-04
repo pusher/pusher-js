05568d5c3a54caf955b2fe2c65083cd2a7a90af4
Paweł Ledwoń <me@ludvik.pl>
2012-12-04 16:20:40 +0000

    Implement ConnectionManager partially
    * incomplete close code handling
    * no way to force ssl on
    * no support for netinfo

---
+++ spec/javascripts/unit/connection/connection_manager_spec.js
@@ -0,0 +1,319 @@
+describe("ConnectionManager", function() {
+  beforeEach(function() {
+    var self = this;
+
+    this.transport = new Object();
+
+    this.connection = new Pusher.EventsDispatcher();
+    this.connection.supportsPing = jasmine.createSpy("supportsPing");
+    this.connection.send = jasmine.createSpy("send")
+      .andReturn(true);
+    this.connection.send_event = jasmine.createSpy("send_event")
+      .andReturn(true);
+    this.connection.close = jasmine.createSpy("close");
+
+    this.strategy = new Pusher.EventsDispatcher();
+
+    this.strategy.isSupported = jasmine.createSpy("isSupported")
+      .andReturn(true);
+    this.strategy.initialize = jasmine.createSpy("initialize");
+    this.strategy.connect = jasmine.createSpy("connect");
+    this.strategy.abort = jasmine.createSpy("abort");
+
+    spyOn(Pusher.StrategyBuilder, "build").andReturn(this.strategy);
+    spyOn(window, "setTimeout").andReturn(666);
+    spyOn(window, "clearTimeout");
+
+    this.manager = new Pusher.ConnectionManager("foo", {
+      activityTimeout: 111,
+      pongTimeout: 222,
+      unavailableTimeout: 333
+    });
+    this.manager.wrapTransport = jasmine.createSpy("wrapTransport")
+      .andReturn(this.connection);
+  });
+
+  describe("on initialization", function() {
+    it("should transition to initialized state", function() {
+      expect(this.manager.state).toEqual("initialized");
+    });
+
+    it("should pass the key to the strategy builder", function() {
+      expect(Pusher.StrategyBuilder.build.calls[0].args[0].key)
+        .toEqual("foo");
+    });
+  });
+
+  describe("on connecting", function() {
+    it("should initialize strategy and try to connect", function() {
+      this.manager.connect();
+      expect(this.strategy.initialize).toHaveBeenCalled();
+      expect(this.strategy.connect).toHaveBeenCalled();
+    });
+
+    it("should transition to connecting", function() {
+      var onConnecting = jasmine.createSpy("onConnecting");
+      var onStateChange = jasmine.createSpy("onStateChange");
+      this.manager.bind("connecting", onConnecting);
+      this.manager.bind("state_change", onStateChange);
+
+      this.manager.connect();
+
+      expect(this.manager.state).toEqual("connecting");
+      expect(onConnecting).toHaveBeenCalled();
+      expect(onStateChange).toHaveBeenCalledWith({
+        previous: "initialized",
+        current: "connecting"
+      });
+    });
+
+    it("should set the unavailable timer", function() {
+      this.manager.connect();
+      expect(setTimeout).toHaveBeenCalledWith(jasmine.any(Function), 333);
+    });
+  });
+
+  describe("after successful connection attempt", function() {
+    it("should transition to connected", function() {
+      var onConnected = jasmine.createSpy("onConnected");
+      this.manager.bind("connected", onConnected);
+
+      this.manager.connect();
+      this.strategy.emit("open", this.transport);
+
+      expect(onConnected).not.toHaveBeenCalled();
+      this.connection.emit("connected", "123.456");
+
+      expect(onConnected).toHaveBeenCalled();
+      expect(this.manager.socket_id).toEqual("123.456");
+    });
+
+    it("should abort substrategy immediately", function() {
+      this.manager.connect();
+      expect(this.strategy.abort).not.toHaveBeenCalled();
+
+      this.strategy.emit("open", this.transport);
+      expect(this.strategy.abort).toHaveBeenCalled();
+    });
+
+    it("should clear the unavailable timer", function() {
+      setTimeout.andReturn(123);
+
+      this.manager.connect();
+      this.strategy.emit("open", this.transport);
+
+      expect(clearTimeout).toHaveBeenCalledWith(123);
+    });
+
+    it("should not try to connect again", function() {
+      this.manager.connect();
+      this.strategy.emit("open", this.transport);
+
+      expect(this.strategy.initialize.calls.length).toEqual(1);
+      expect(this.strategy.connect.calls.length).toEqual(1);
+
+      this.manager.connect();
+
+      expect(this.strategy.initialize.calls.length).toEqual(1);
+      expect(this.strategy.connect.calls.length).toEqual(1);
+    });
+  });
+
+  describe("when sending messages", function() {
+    it("should pass data to the transport", function() {
+      this.manager.connect();
+      this.strategy.emit("open", this.transport);
+      this.connection.emit("connected", "123.456");
+      expect(this.manager.send("howdy")).toBe(true);
+
+      expect(this.connection.send).toHaveBeenCalledWith("howdy");
+    });
+
+
+    it("should not send data when not connected", function() {
+      expect(this.manager.send("FALSE!")).toBe(false);
+    });
+  });
+
+  describe("on requested disconnection", function() {
+    it("should transition to disconnected", function() {
+      var onDisconnected = jasmine.createSpy("onDisconnected");
+      this.manager.bind("disconnected", onDisconnected);
+
+      this.manager.connect();
+      this.manager.disconnect();
+
+      expect(onDisconnected).toHaveBeenCalled();
+    });
+
+    it("should close the connection", function() {
+      this.manager.connect();
+      this.strategy.emit("open", this.transport);
+      this.manager.disconnect();
+
+      expect(this.connection.close).toHaveBeenCalled();
+    });
+
+    it("should abort the connection attempt", function() {
+      this.manager.connect();
+      this.manager.disconnect();
+
+      expect(this.strategy.abort).toHaveBeenCalled();
+    });
+
+    it("should clear unavailable timer and activity check", function() {
+      this.manager.connect();
+      this.strategy.emit("open", this.transport)
+      this.manager.disconnect();
+
+      expect(clearTimeout.calls.length).toEqual(2);
+    });
+  });
+
+  describe("on unexpected disconnection", function() {
+    it("should transition to disconnected then to connecting", function() {
+      var self = this;
+
+      this.manager.connect();
+      this.strategy.emit("open", this.transport);
+
+      var onConnecting = jasmine.createSpy("onConnecting");
+      var onDisconnected = jasmine.createSpy("onDisconnected")
+        .andCallFake(function() {
+          self.manager.bind("connecting", onConnecting);
+        });
+      this.manager.bind("disconnected", onDisconnected);
+
+      this.connection.emit("closed");
+
+      expect(onDisconnected).toHaveBeenCalled();
+      expect(onConnecting).toHaveBeenCalled();
+    });
+
+    it("should clean up timers and abort strategy", function() {
+      var self = this;
+
+      this.manager.connect();
+      this.strategy.emit("open", this.transport);
+      // unavailable timer should be cleared here
+      expect(clearTimeout.calls.length).toEqual(1);
+
+      this.connection.emit("closed");
+
+      expect(this.strategy.abort).toHaveBeenCalled();
+      // activity check should be cleared here
+      // unavailable timer was cleared when connection was open
+      expect(clearTimeout.calls.length).toEqual(2);
+    });
+  });
+
+  describe("on reconnection", function() {
+    it("should use the same strategy to reconnect", function() {
+      var onConnected = jasmine.createSpy("onConnected");
+      this.manager.bind("connected", onConnected);
+
+      this.manager.connect();
+
+      expect(this.strategy.initialize.calls.length).toEqual(1);
+      expect(this.strategy.connect.calls.length).toEqual(1);
+
+      this.strategy.emit("open", this.transport);
+      this.connection.emit("connected", "123.456");
+
+      expect(onConnected.calls.length).toEqual(1);
+      expect(this.manager.socket_id).toEqual("123.456");
+
+      this.manager.disconnect();
+      this.manager.connect();
+
+      expect(this.strategy.initialize.calls.length).toEqual(2);
+      expect(this.strategy.connect.calls.length).toEqual(2);
+
+      this.strategy.emit("open", this.transport);
+      this.connection.emit("connected", "666.999");
+
+      expect(onConnected.calls.length).toEqual(2);
+      expect(this.manager.socket_id).toEqual("666.999");
+    });
+  });
+
+  describe("on unavailable timeout", function() {
+    it("should fire the timer and transition to unavailable", function() {
+      this.manager.connect();
+      expect(this.manager.state).toEqual("connecting");
+
+      var onUnavailable = jasmine.createSpy("onUnavailable");
+      this.manager.bind("unavailable", onUnavailable);
+
+      setTimeout.calls[0].args[0].call(window);
+      expect(this.manager.state).toEqual("unavailable");
+      expect(onUnavailable).toHaveBeenCalled();
+    });
+  });
+
+  describe("on activity timeout", function() {
+    it("should send a pusher:ping event", function() {
+      this.manager.connect();
+      this.strategy.emit("open", this.transport);
+      this.connection.emit("connected", "666.999");
+
+      // on connection open and on pusher:connection_established
+      expect(setTimeout.calls.length).toEqual(3);
+      expect(clearTimeout.calls.length).toEqual(2);
+      // call the activity timer
+      setTimeout.calls[2].args[0].call(window);
+
+      expect(this.connection.send_event)
+        .toHaveBeenCalledWith("pusher:ping", {}, undefined);
+      // set the pong timeout
+      expect(setTimeout.calls.length).toEqual(4);
+      expect(clearTimeout.calls.length).toEqual(2);
+
+      this.connection.emit("pong");
+      this.connection.emit("message", {
+        event: "pusher:pong",
+        data: {}
+      });
+      // clear the pong timeout
+      expect(clearTimeout.calls.length).toEqual(3);
+      // set the new activity timeout
+      expect(setTimeout.calls.length).toEqual(5);
+    });
+
+    it("should close the connection on timeout", function() {
+      this.manager.connect();
+      this.strategy.emit("open", this.transport);
+
+      setTimeout.calls[1].args[0].call(window);
+
+      expect(this.connection.close).not.toHaveBeenCalled();
+      setTimeout.calls[2].args[0].call(window);
+      expect(this.connection.close).toHaveBeenCalled();
+    });
+  });
+
+  describe("on ping", function() {
+    it("should reply with a pusher:pong event", function() {
+      this.manager.connect();
+      this.strategy.emit("open", this.transport);
+      this.connection.emit("connected", "666.999");
+
+      this.connection.emit("ping");
+      expect(this.connection.send_event)
+        .toHaveBeenCalledWith("pusher:pong", {}, undefined);
+    });
+  });
+
+  describe("with unsupported strategy", function() {
+    it("should transition to failed on connect", function() {
+      this.strategy.isSupported = jasmine.createSpy("isSupported")
+        .andReturn(false)
+
+      var onFailed = jasmine.createSpy("onFailed");
+      this.manager.bind("failed", onFailed);
+
+      this.manager.connect();
+      expect(onFailed).toHaveBeenCalled();
+    });
+  });
+});
---
+++ src/connection/connection_manager.js
@@ -0,0 +1,179 @@
+;(function() {
+
+  // TODO use netinfo
+  // TODO compulsory secure
+  // TODO chrome connected but offline
+
+  function ConnectionManager(key, options) {
+    Pusher.EventsDispatcher.call(this);
+
+    this.options = options || {};
+    this.state = "initialized";
+    this.connection = null;
+
+    this.strategy = Pusher.StrategyBuilder.build(
+      Pusher.Util.extend(Pusher.defaultStrategy, { key: key })
+    );
+
+    var self = this;
+    this.strategy.bind("open", function(transport) {
+      // we don't support switching connections yet
+      self.strategy.abort();
+      self.clearUnavailableTimer();
+      self.setConnection(self.wrapTransport(transport));
+    });
+  };
+  var prototype = ConnectionManager.prototype;
+
+  Pusher.Util.extend(prototype, Pusher.EventsDispatcher.prototype);
+
+  // interface
+
+  prototype.connect = function() {
+    if (this.connection) {
+      return;
+    }
+    if (this.state === "connecting") {
+      return;
+    }
+    if (!this.strategy.isSupported()) {
+      this.updateState("failed");
+    }
+
+    this.updateState("connecting");
+    this.strategy.initialize();
+    this.strategy.connect();
+
+    var self = this;
+    this.unavailableTimer = setTimeout(function() {
+      if (!self.unavailableTimer) {
+        return;
+      }
+      self.updateState("unavailable");
+      self.unavailableTimer = null;
+    }, this.options.unavailableTimeout);
+  };
+
+  prototype.send = function(data) {
+    if (this.connection) {
+      return this.connection.send(data);
+    } else {
+      return false;
+    }
+  };
+
+  prototype.send_event = function(name, data, channel) {
+    if (this.connection) {
+      return this.connection.send_event(name, data, channel);
+    } else {
+      return false;
+    }
+  };
+
+  prototype.disconnect = function(data) {
+    this.clearUnavailableTimer();
+    this.stopActivityCheck();
+    this.strategy.abort();
+    this.updateState("disconnected");
+    // we're in disconnected state, so closing will not cause reconnecting
+    if (this.connection) {
+      this.connection.close();
+      this.connection = null;
+    }
+  };
+
+  // private
+
+  prototype.wrapTransport = function(transport) {
+    return new Pusher.ProtocolWrapper(transport);
+  };
+
+  prototype.clearUnavailableTimer = function() {
+    if (this.unavailableTimer) {
+      clearTimeout(this.unavailableTimer);
+      this.unavailableTimer = null;
+    }
+  };
+
+  prototype.resetActivityCheck = function() {
+    this.stopActivityCheck();
+    // send ping after inactivity
+    if (!this.connection.supportsPing()) {
+      var self = this;
+      this.activityTimer = setTimeout(function() {
+        self.send_event('pusher:ping', {})
+        // wait for pong response
+        self.activityTimer = setTimeout(function() {
+          self.connection.close();
+        }, (self.options.pongTimeout));
+      }, (this.options.activityTimeout));
+    }
+  };
+
+  prototype.stopActivityCheck = function() {
+    if (this.activityTimer) {
+      clearTimeout(this.activityTimer);
+      this.activityTimer = null;
+    }
+  }
+
+  prototype.setConnection = function(connection) {
+    this.connection = connection;
+
+    var self = this;
+    var onConnected = function(id) {
+      self.socket_id = id;
+      self.updateState("connected");
+      self.resetActivityCheck();
+    };
+    var onMessage = function(message) {
+      // includes pong messages from server
+      self.resetActivityCheck();
+      self.emit('message', message);
+    };
+    var onPing = function() {
+      self.send_event('pusher:pong', {});
+    };
+    var onError = function() {
+      // just emit error to user - socket will already be closed by browser
+      self.emit("error", { type: "WebSocketError", error: error });
+    };
+    var onClosed = function() {
+      connection.unbind("connected", onConnected);
+      connection.unbind("message", onMessage);
+      connection.unbind("ping", onPing);
+      connection.unbind("error", onError);
+      connection.unbind("closed", onClosed);
+      self.connection = null;
+
+      if (self.state !== "disconnected") {
+        self.disconnect();
+        self.connect();
+      }
+    };
+
+    connection.bind("connected", onConnected);
+    connection.bind("message", onMessage);
+    connection.bind("ping", onPing);
+    connection.bind("error", onError);
+    connection.bind("closed", onClosed);
+
+    this.resetActivityCheck();
+  };
+
+  prototype.updateState = function(newState, data) {
+    var previousState = this.state;
+
+    this.state = newState;
+    // Only emit when the state changes
+    if (previousState !== newState) {
+      // TODO nicer debug
+      Pusher.debug('State changed', previousState + ' -> ' + newState);
+
+      this.emit('state_change', { previous: previousState, current: newState });
+      this.emit(newState, data);
+    }
+  }
+
+  Pusher.ConnectionManager = ConnectionManager;
+}).call(this);
