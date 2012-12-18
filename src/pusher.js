bd5ad6fb05e9234d0b073c031ba138ac8477eb5d
Paweł Ledwoń <me@ludvik.pl>
2012-12-18 15:18:59 +0000

    Add mapObject and flatten functions to Pusher.Util

--- src/pusher.js
+++ src/pusher.js
@@ -219,6 +219,27 @@
       return result;
     },
 
+    /** Maps all elements of the object and returns the result.
+     *
+     * Function f gets 4 arguments passed:
+     * - element from the object
+     * - key of the element
+     * - reference to the source object
+     * - reference to the destination object
+     *
+     * @param {Object} object
+     * @param {Function} f
+     */
+    mapObject: function(object, f) {
+      var result = {};
+      for (var key in object) {
+        if (Object.prototype.hasOwnProperty.call(object, key)) {
+          result[key] = f(object[key]);
+        }
+      }
+      return result;
+    },
+
     /** Filters elements of the array using a test function.
      *
      * Function test gets 4 arguments passed:
@@ -235,6 +256,21 @@
       for (var i = 0; i < array.length; i++) {
         if (test(array[i], i, array, result)) {
           result.push(array[i]);
+        }
+      }
+      return result;
+    },
+
+    /** Flattens an object into a two-dimensional array.
+     *
+     * @param  {Object} object
+     * @return {Array} resulting array of [key, value] pairs
+     */
+    flatten: function(object) {
+      var result = [];
+      for (var key in object) {
+        if (Object.prototype.hasOwnProperty.call(object, key)) {
+          result.push([key, object[key]]);
         }
       }
       return result;
