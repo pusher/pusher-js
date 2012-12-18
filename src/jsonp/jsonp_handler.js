529d95c73d270e048d98f840ac19a37534a4fb2b
Paweł Ledwoń <me@ludvik.pl>
2012-12-18 15:20:06 +0000

    Implement a simple JSONP handler

---
+++ src/jsonp/jsonp_handler.js
@@ -0,0 +1,36 @@
+(function() {
+
+  function JSONPHandler(options) {
+    this.index = 0;
+    this.callbacks = {};
+    this.options = options;
+  }
+
+  var prototype = JSONPHandler.prototype;
+
+  prototype.send = function(data, callback) {
+    var id = this.index;
+    var request = new Pusher.JSONPRequest(id, this.options);
+
+    this.callbacks[id] = function(error, result) {
+      request.receive(error, result, callback);
+    };
+
+    request.send(data);
+  };
+
+  prototype.receive = function(id, data) {
+    if (this.callbacks[id]) {
+      this.callbacks[id](null, data);
+      delete this.callbacks[id];
+    }
+  };
+
+  Pusher.JSONPHandler = JSONPHandler;
+  Pusher.JSONP = new JSONPHandler({
+    url: "http://localhost:4567/jsonp",
+    prefix: "_pusher_jsonp_",
+    receiver: "Pusher.JSONP.receive"
+  });
+
+}).call(this);
---
+++ src/jsonp/jsonp_request.js
@@ -0,0 +1,51 @@
+(function() {
+
+  function JSONPRequest(id, options) {
+    this.id = 0;
+    this.options = options;
+  }
+
+  var prototype = JSONPRequest.prototype;
+
+  prototype.send = function(data) {
+    var params = Pusher.Util.extend(
+      {}, data, { receiver: this.options.receiver }
+    );
+    var query = Pusher.Util.map(Pusher.Util.flatten(params), function(pair) {
+      return pair[0] + "=" + encodeValue(pair[1]);
+    }).join("&");
+
+    var script = document.createElement("script");
+    script.id = this.options.prefix + this.id;
+    script.src = this.options.url + "/" + this.id + "?" + query;
+    script.type = "text/javascript";
+    script.charset = "UTF-8";
+
+    var head = document.getElementsByTagName('head')[0];
+    head.insertBefore(script, head.firstChild);
+
+    return true;
+  };
+
+  prototype.receive = function(error, result, callback) {
+    callback(error, Pusher.Util.mapObject(result, decodeValue));
+  }
+
+  function encodeValue(value) {
+    if (typeof value === "object") {
+      value = JSON.stringify(value);
+    }
+    return encodeURIComponent(value);
+  }
+
+  function decodeValue(value) {
+    try {
+      return JSON.parse(value);
+    } catch(e) {
+      return value;
+    }
+  }
+
+  Pusher.JSONPRequest = JSONPRequest;
+
+}).call(this);
