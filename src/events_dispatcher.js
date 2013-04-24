304fa113f662e87320eedfef92447c7819e0b7be
Paweł Ledwoń <me@ludvik.pl>
2013-04-24 12:21:56 +0100

    Adjust last fix to coding standards

--- src/events_dispatcher.js
+++ src/events_dispatcher.js
@@ -63,12 +63,11 @@
   CallbackRegistry.prototype.remove = function(eventName, callback) {
     if(this.get(eventName)) {
       var index = Pusher.Util.arrayIndexOf(this.get(eventName), callback);
-      if(index == -1){
-          return;
+      if (index !== -1){
+        var callbacksCopy = this._callbacks[this._prefix(eventName)].slice(0);
+        callbacksCopy.splice(index, 1);
+        this._callbacks[this._prefix(eventName)] = callbacksCopy;
       }
-      var callbacksCopy = this._callbacks[this._prefix(eventName)].slice(0);
-      callbacksCopy.splice(index, 1);
-      this._callbacks[this._prefix(eventName)] = callbacksCopy;
     }
   };
 
