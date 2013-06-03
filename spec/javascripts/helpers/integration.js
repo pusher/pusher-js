9763adf74242c8d594051d02b2183773fac4eac2
Paweł Ledwoń <me@ludvik.pl>
2013-06-03 16:23:35 +0200

    Clean up integration tests

--- spec/javascripts/helpers/integration.js
+++ spec/javascripts/helpers/integration.js
@@ -10,3 +10,19 @@ Pusher.Integration = {};
 
 Pusher.Integration.API_URL = "http://js-integration-api.pusher.com";
 Pusher.Integration.JS_HOST = "http://localhost:5555";
+
+Pusher.Integration.getRandomName = function(prefix) {
+  return prefix + "_" + Pusher.Util.now() + "_" + Math.floor(Math.random() * 1000000);
+}
+
+Pusher.Integration.sendAPIMessage = function(request) {
+  Pusher.JSONPRequest.send({
+    data: {
+      channel: request.channel,
+      event: request.event,
+      data: request.data
+    },
+    url: request.url,
+    receiver: request.receiver || Pusher.JSONP
+  }, function() {});
+}
