09a1332f04eae8e21ac51bf4131f92aa9a8a221e
Paweł Ledwoń <me@ludvik.pl>
2013-08-08 19:12:12 +0100

    Update configuration for karma 0.10

--- spec/karma/config/unit.js
+++ spec/karma/config/unit.js
@@ -1,76 +1,37 @@
-// Testacular configuration
-// Generated on Fri Mar 15 2013 15:43:53 GMT+0000 (GMT)
+module.exports = function(config) {
+  config.set({
+    basePath: '../../../',
+    frameworks: ["jasmine"],
 
+    files: []
+      .concat(require(__dirname + "/../files/source"))
+      .concat(require(__dirname + "/../files/unit_tests")),
+    exclude: [
+      'src/sockjs/**/*',
+      'src/web-socket-js/**/*'
+    ],
 
-// base path, that will be used to resolve files and exclude
-basePath = '../../../';
+    preprocessors: {
+      '**/src/**/*.js': 'coverage'
+    },
+    reporters: ['progress', 'coverage'],
 
+    coverageReporter: {
+      type : 'html',
+      dir : 'coverage/'
+    },
 
-// list of files / patterns to load in the browser
-files = [JASMINE, JASMINE_ADAPTER]
-  .concat(require(__dirname + "/../files/source"))
-  .concat(require(__dirname + "/../files/unit_tests"));
+    port: 9876,
+    runnerPort: 9100,
 
+    colors: true,
+    logLevel: config.LOG_INFO,
 
-// list of files to exclude
-exclude = [
-  'src/sockjs/**/*',
-  'src/web-socket-js/**/*'
-];
+    autoWatch: true,
 
+    browsers: ['Chrome', 'Firefox', 'Opera', 'Safari'],
+    captureTimeout: 60000,
 
-preprocessors = {
-  '**/src/**/*.js': 'coverage'
+    singleRun: false
+  });
 };
-
-
-// test results reporter to use
-// possible values: 'dots', 'progress', 'junit'
-reporters = ['progress', 'coverage'];
-
-
-coverageReporter = {
-  type : 'html',
-  dir : 'coverage/'
-};
-
-
-// web server port
-port = 9876;
-
-
-// cli runner port
-runnerPort = 9100;
-
-
-// enable / disable colors in the output (reporters and logs)
-colors = true;
-
-
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
-
-
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = true;
-
-
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['Chrome', 'Firefox', 'Opera', 'Safari'];
-
-
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 60000;
-
-
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
