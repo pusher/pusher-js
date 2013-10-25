a64ee604ebae01e53da23fabd3c1aa62a8126e9e
Paweł Ledwoń <me@ludvik.pl>
2013-10-25 11:56:18 +0200

    Use the production CDN in distribution files

--- dist/pusher.js
+++ dist/pusher.js
@@ -530,8 +530,8 @@
   Pusher.pong_timeout = 30000;
   Pusher.unavailable_timeout = 10000;
   // CDN configuration
-  Pusher.cdn_http = 'http://localhost:5555/';
-  Pusher.cdn_https = 'https://localhost:5555/';
+  Pusher.cdn_http = 'http://js.pusher.com/';
+  Pusher.cdn_https = 'https://d3dy5gmtp8yhk7.cloudfront.net/';
   Pusher.dependency_suffix = '';
 
   Pusher.getDefaultStrategy = function(config) {
