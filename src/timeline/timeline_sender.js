e5d1978cd17b834a5332c27be2467efa0093b7db
Jamie Patel <jamie@notespublication.com>
2015-12-10 22:42:12 +0000

    Port stats to xhr

--- src/timeline/timeline_sender.js
+++ src/timeline/timeline_sender.js
@@ -1,4 +1,5 @@
 var Util = require('../util');
+var Base64 = require('../base64');
 
 function TimelineSender(timeline, options) {
   this.timeline = timeline;
@@ -16,9 +17,38 @@ prototype.send = function(encrypted, callback) {
   var sendXHR = function(data, callback) {
     var scheme = "http" + (encrypted ? "s" : "") + "://";
     var url = scheme + (self.host || self.options.host) + self.options.path;
-    // UNFINISHED
+    var params = Util.filterObject(data, function(value) {
+      return value !== undefined;
+    });
+
+    var query = Util.map(
+      Util.flatten(encodeParamsObject(params)),
+      Util.method("join", "=")
+    ).join("&");
+
+    url += ("/" + 2 + "?" + query); // TODO: check what to do in lieu of receiver number
+
+    var xhr = Util.createXHR();
+    xhr.open("GET", url, true);
+
+    xhr.onreadystatechange = function(){
+      if (xhr.readyState === 4) {
+        // TODO: handle response
+      }
+    }
+
+    xhr.send()
   };
   self.timeline.send(sendXHR, callback);
 };
+
+function encodeParamsObject(data) {
+  return Util.mapObject(data, function(value) {
+    if (typeof value === "object") {
+      value = JSON.stringify(value);
+    }
+    return encodeURIComponent(Base64.encode(value.toString()));
+  });
+}
 
 module.exports = TimelineSender;
