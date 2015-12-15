848e87992a6bd8730b7564617611a8656d9d8773
Jamie Patel <jamie@notespublication.com>
2015-12-15 15:04:02 +0000

    Add UMD web build

---
+++ webpack/config.web-umd.js
@@ -0,0 +1,34 @@
+var path = require("path");
+var NormalModuleReplacementPlugin = require('webpack').NormalModuleReplacementPlugin;
+var version = require('../package').version;
+
+///////////////////////////////////////////////////
+// The web build uses:                           //
+// XHR, WebSocket and NetInfo in platforms/web/* //
+///////////////////////////////////////////////////
+module.exports = {
+  entry: "./src/pusher",
+  output: {
+    library: "Pusher",
+    path: path.join(__dirname, "../dist/web-umd"),
+    filename: "pusher.js",
+    libraryTarget: "umd"
+  },
+  externals: {
+    '../package': '{version: "'+ version +'"}'
+  },
+  plugins: [
+    new NormalModuleReplacementPlugin(
+      /^pusher-websocket-js-iso-externals-node\/ws$/,
+      "pusher-websocket-js-iso-externals-web/ws"
+    ),
+    new NormalModuleReplacementPlugin(
+      /^pusher-websocket-js-iso-externals-node\/xhr$/,
+      "pusher-websocket-js-iso-externals-web/xhr"
+    ),
+    new NormalModuleReplacementPlugin(
+      /^pusher-websocket-js-iso-externals-node\/net_info$/,
+      "pusher-websocket-js-iso-externals-web/net_info"
+    )
+  ]
+}
