07ca201338cd34efca261b13b293228f822c8441
Jamie Patel <jamie@notespublication.com>
2016-05-11 10:49:21 +0100

    Build for 3.1

--- dist/react-native/pusher.js
+++ dist/react-native/pusher.js
@@ -1,5 +1,5 @@
 /*!
- * Pusher JavaScript Library v3.1.0-pre11
+ * Pusher JavaScript Library v3.1.0
  * http://pusher.com/
  *
  * Copyright 2016, Pusher
@@ -776,7 +776,7 @@ module.exports =
 
 	"use strict";
 	var Defaults = {
-	    VERSION: "3.1.0-pre11",
+	    VERSION: "3.1.0",
 	    PROTOCOL: 7,
 	    host: 'ws.pusherapp.com',
 	    ws_port: 80,
