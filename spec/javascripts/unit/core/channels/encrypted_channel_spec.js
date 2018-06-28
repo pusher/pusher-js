f27466c2a78931899ab1d3283106f076e2eae823
James Lees <james.lees@pusher.com>
2018-06-28 12:23:13 +0100

    squash me

--- spec/javascripts/unit/core/channels/encrypted_channel_spec.js
+++ spec/javascripts/unit/core/channels/encrypted_channel_spec.js
@@ -46,12 +46,12 @@ describe("EncryptedChannel", function() {
 
     beforeEach(() => {
       authorizer = Mocks.getAuthorizer();
-      factorySpy = spyOn(Factory, "createAuthorizer").and.returnValue(authorizer);
+      factorySpy = spyOn(Factory, "createAuthorizer").andReturn(authorizer);
     });
 
     it("should create and call an authorizer", function() {
       channel.authorize("1.23", function() {});
-      expect(Factory.createAuthorizer.calls.count()).toEqual(1);
+      expect(Factory.createAuthorizer.calls.length).toEqual(1);
       expect(Factory.createAuthorizer).toHaveBeenCalledWith(
         channel,
         { foo: "bar" }
@@ -80,7 +80,7 @@ describe("EncryptedChannel", function() {
           }
         });
         channel = new EncryptedChannel("private-test-custom-auth", pusher);
-        factorySpy.and.callThrough();
+        factorySpy.andCallThrough();
       });
 
       it("should call the authorizer", function() {
@@ -99,10 +99,10 @@ describe("EncryptedChannel", function() {
     it("should raise an exception if the event name does not start with client-", function() {
       expect(function() {
         channel.trigger("whatever", {});
-      }).toThrow(jasmine.any(Error));
-      //}).toThrow(jasmine.any(Errors.BadEventName)
+      }).toThrow();
       // There is some weird bundling stuff causing this e to not be an
       // instanceof the Errors.BadEventName class
+      //}).toThrow(jasmine.any(Errors.BadEventName)
     });
 
     it("should call send_event on connection", function() {
@@ -112,12 +112,12 @@ describe("EncryptedChannel", function() {
     });
 
     it("should return true if connection sent the event", function() {
-      pusher.send_event.and.returnValue(true);
+      pusher.send_event.andReturn(true);
       expect(channel.trigger("client-test", {})).toBe(true);
     });
 
     it("should return false if connection didn't send the event", function() {
-      pusher.send_event.and.returnValue(false);
+      pusher.send_event.andReturn(false);
       expect(channel.trigger("client-test", {})).toBe(false);
     });
   });
@@ -127,28 +127,35 @@ describe("EncryptedChannel", function() {
       // in order to trigger encrypted events, we need to get a shared secret
       // from the authorizer.
       var authorizer = Mocks.getAuthorizer();
-      factorySpy = spyOn(Factory, "createAuthorizer").and.returnValue(authorizer);
+      factorySpy = spyOn(Factory, "createAuthorizer").andReturn(authorizer);
       var callback = () => {}
       channel.authorize("1.23", callback);
       authorizer._callback(false, {
         shared_secret: secretBase64,
         foo: "bar"
       });
-      randomBytesSpy = spyOn(tweetNacl, 'randomBytes').and.returnValue(nonceBytes)
+      randomBytesSpy = spyOn(tweetNacl, 'randomBytes').andReturn(nonceBytes)
 
     });
     it("should raise an exception if the event name does not start with client-", function(done) {
       let thenFn = jasmine.createSpy()
-      channel.triggerEncrypted("whatever", {})
-        .then(thenFn)
-        .catch(e =>{
-          expect(thenFn).not.toHaveBeenCalled();
-          // expect(e).toBe(jasmine.any(Errors.BadEventName))
-          // There is some weird bundling stuff causing this e to not be an
-          // instanceof the Errors.BadEventName class
-          expect(e.message).toBe("Event 'whatever' does not start with 'client-'");
-          done();
-        });
+      let catchFn = jasmine.createSpy()
+      runs(() => {
+        channel.triggerEncrypted("whatever", {}).then(thenFn).catch(catchFn);
+      });
+      waitsFor(() => {
+        return catchFn.calls.length > 0;
+      }, "catch function to be called", 5000)
+      runs(() => {
+        expect(thenFn).not.toHaveBeenCalled();
+        expect(catchFn).toHaveBeenCalled();
+        let err = catchFn.calls[0].args[0];
+
+        // There is some weird bundling stuff causing this e to not be an
+        // instanceof the Errors.BadEventName class
+        // expect(err).toBe(jasmine.any(Errors.BadEventName))
+        expect(err.message).toBe("Event 'whatever' does not start with 'client-'");
+      });
     });
 
     it("should call send_event on connection with encrypted data", function(done) {
@@ -165,14 +172,14 @@ describe("EncryptedChannel", function() {
 
     // maybe this promise should just resolve if it sends and reject if not?
     it("should return a promise resolving to true if connection sent the event", function() {
-      pusher.send_event.and.returnValue(true);
+      pusher.send_event.andReturn(true);
       channel.triggerEncrypted("client-test", {}).then((val) => {
         expect(val).toBe(true);
       })
     });
 
     it("should return false if connection didn't send the event", function() {
-      pusher.send_event.and.returnValue(false);
+      pusher.send_event.andReturn(false);
       channel.triggerEncrypted("client-test", {}).then((val) => {
         expect(val).toBe(false);
       })
@@ -261,14 +268,14 @@ describe("EncryptedChannel", function() {
         // in order to decrypt encrypted events, we need to get a shared secret
         // from the authorizer.
         var authorizer = Mocks.getAuthorizer();
-        factorySpy = spyOn(Factory, "createAuthorizer").and.returnValue(authorizer);
+        factorySpy = spyOn(Factory, "createAuthorizer").andReturn(authorizer);
         var callback = () => {}
         channel.authorize("1.23", callback);
         authorizer._callback(false, {
           shared_secret: secretBase64,
           foo: "bar"
         });
-        let randomBytesSpy = spyOn(tweetNacl, 'randomBytes').and.returnValue(nonceBytes)
+        let randomBytesSpy = spyOn(tweetNacl, 'randomBytes').andReturn(nonceBytes)
       });
       it("should decrypt the event payload and emit the event", function(done) {
         let payload = {test: "payload"};
