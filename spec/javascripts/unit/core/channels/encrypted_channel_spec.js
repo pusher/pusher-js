7491ac4425aa57c8da16d329430b1713a4dc6031
James Lees <james.lees@pusher.com>
2018-07-09 10:20:53 +0100

    Use `function` consistently instead of =>

--- spec/javascripts/unit/core/channels/encrypted_channel_spec.js
+++ spec/javascripts/unit/core/channels/encrypted_channel_spec.js
@@ -1,29 +1,29 @@
-const Authorizer = require('core/auth/pusher_authorizer').default;
-const Errors = require('core/errors');
-const EncryptedChannel = require('core/channels/encrypted_channel').default;
-const Factory = require('core/utils/factory').default;
+const Authorizer = require("core/auth/pusher_authorizer").default;
+const Errors = require("core/errors");
+const EncryptedChannel = require("core/channels/encrypted_channel").default;
+const Factory = require("core/utils/factory").default;
 const Mocks = require("mocks");
-const tweetNacl = require('tweetnacl');
-const tweetNaclUtil = require('tweetnacl-util');
+const tweetNacl = require("tweetnacl");
+const tweetNaclUtil = require("tweetnacl-util");
 const timeout = 5000;
 
 describe("EncryptedChannel", function() {
   var pusher;
   var channel;
   var factorySpy;
-  const secretUTF8 = 'It Must Be Thirty Two Characters';
-  const secretBytes = tweetNaclUtil.decodeUTF8(secretUTF8)
+  const secretUTF8 = "It Must Be Thirty Two Characters";
+  const secretBytes = tweetNaclUtil.decodeUTF8(secretUTF8);
   const secretBase64 = tweetNaclUtil.encodeBase64(secretBytes);
-  const nonceUTF8 = 'aaaaaaaaaaaaaaaaaaaaaaaa';
-  const nonceBytes = tweetNaclUtil.decodeUTF8(nonceUTF8)
+  const nonceUTF8 = "aaaaaaaaaaaaaaaaaaaaaaaa";
+  const nonceBytes = tweetNaclUtil.decodeUTF8(nonceUTF8);
   const nonceBase64 = tweetNaclUtil.encodeBase64(nonceBytes);
-  const testEncrypt = (payload) => {
+  const testEncrypt = function(payload) {
     let payloadBytes = tweetNaclUtil.decodeUTF8(JSON.stringify(payload));
-    let bytes = tweetNacl.secretbox(payloadBytes, nonceBytes, secretBytes)
-    return tweetNaclUtil.encodeBase64(bytes)
-  }
+    let bytes = tweetNacl.secretbox(payloadBytes, nonceBytes, secretBytes);
+    return tweetNaclUtil.encodeBase64(bytes);
+  };
 
-  beforeEach(() => {
+  beforeEach(function() {
     pusher = Mocks.getPusher({ foo: "bar" });
     channel = new EncryptedChannel("private-encrypted-test", pusher);
   });
@@ -45,7 +45,7 @@ describe("EncryptedChannel", function() {
   describe("#authorize", function() {
     let authorizer;
 
-    beforeEach(() => {
+    beforeEach(function() {
       authorizer = Mocks.getAuthorizer();
       factorySpy = spyOn(Factory, "createAuthorizer").andReturn(authorizer);
     });
@@ -53,10 +53,9 @@ describe("EncryptedChannel", function() {
     it("should create and call an authorizer", function() {
       channel.authorize("1.23", function() {});
       expect(Factory.createAuthorizer.calls.length).toEqual(1);
-      expect(Factory.createAuthorizer).toHaveBeenCalledWith(
-        channel,
-        { foo: "bar" }
-      );
+      expect(Factory.createAuthorizer).toHaveBeenCalledWith(channel, {
+        foo: "bar"
+      });
     });
 
     it("should call back with only authorization data", function() {
@@ -76,13 +75,14 @@ describe("EncryptedChannel", function() {
       authorizer._callback(false, {
         foo: "bar"
       });
-      expect(callback).toHaveBeenCalledWith(true,
+      expect(callback).toHaveBeenCalledWith(
+        true,
         "No shared_secret key in auth payload for encrypted channel: private-encrypted-test"
       );
     });
 
-    describe('with custom authorizer', function() {
-      beforeEach(() => {
+    describe("with custom authorizer", function() {
+      beforeEach(function() {
         pusher = Mocks.getPusher({
           authorizer: function(channel, options) {
             return authorizer;
@@ -106,15 +106,15 @@ describe("EncryptedChannel", function() {
 
   describe("#trigger", function() {
     let randomBytesSpy;
-    beforeEach(() => {
+    beforeEach(function() {
       // in order to trigger encrypted events, we need to get a shared secret
       // from the authorizer.
       let authorizer = Mocks.getAuthorizer();
       factorySpy = spyOn(Factory, "createAuthorizer").andReturn(authorizer);
-      let callback = () => {}
+      let callback = function() {};
       channel.authorize("1.23", callback);
-      authorizer._callback(false, {shared_secret: secretBase64, foo: "bar"});
-      randomBytesSpy = spyOn(tweetNacl, 'randomBytes').andReturn(nonceBytes)
+      authorizer._callback(false, { shared_secret: secretBase64, foo: "bar" });
+      randomBytesSpy = spyOn(tweetNacl, "randomBytes").andReturn(nonceBytes);
     });
     it("should raise an exception if the event name does not start with client-", function() {
       expect(function() {
@@ -123,15 +123,18 @@ describe("EncryptedChannel", function() {
     });
 
     it("should call send_event on connection", function() {
-      let payload = {k: "v"};
+      let payload = { k: "v" };
       let expArg = {
         nonce: nonceBase64,
         ciphertext: testEncrypt(payload)
       };
-      channel.trigger("client-test", payload)
-      expect(pusher.send_event)
-        .toHaveBeenCalledWith("client-test", expArg, "private-encrypted-test");
-      });
+      channel.trigger("client-test", payload);
+      expect(pusher.send_event).toHaveBeenCalledWith(
+        "client-test",
+        expArg,
+        "private-encrypted-test"
+      );
+    });
 
     it("should return true if connection sent the event", function() {
       pusher.send_event.andReturn(true);
@@ -139,7 +142,7 @@ describe("EncryptedChannel", function() {
     });
 
     it("should return false if authentication hasn't completed", function() {
-      let callback = () => {}
+      let callback = function() {};
       channel.authorize("1.23", callback);
       pusher.send_event.andReturn(false);
       expect(channel.trigger("client-test", {})).toBe(false);
@@ -173,18 +176,18 @@ describe("EncryptedChannel", function() {
     describe("on pusher_internal:subscription_succeeded", function() {
       it("should emit pusher:subscription_succeeded", function() {
         let callback = jasmine.createSpy("callback");
-        channel.bind("pusher:subscription_succeeded", callback)
+        channel.bind("pusher:subscription_succeeded", callback);
         channel.handleEvent("pusher_internal:subscription_succeeded", "123");
         expect(callback).toHaveBeenCalledWith("123");
       });
 
       it("should set #subscribed to true", function() {
-        channel.handleEvent("pusher_internal:subscription_succeeded", "123")
+        channel.handleEvent("pusher_internal:subscription_succeeded", "123");
         expect(channel.subscribed).toEqual(true);
       });
 
       it("should set #subscriptionPending to false", function() {
-        channel.handleEvent("pusher_internal:subscription_succeeded", "123")
+        channel.handleEvent("pusher_internal:subscription_succeeded", "123");
         expect(channel.subscriptionPending).toEqual(false);
       });
     });
@@ -194,13 +197,13 @@ describe("EncryptedChannel", function() {
         let callback = jasmine.createSpy("callback");
         channel.bind("pusher:subscription_succeeded", callback);
         channel.cancelSubscription();
-        channel.handleEvent("pusher_internal:subscription_succeeded", "123")
+        channel.handleEvent("pusher_internal:subscription_succeeded", "123");
         expect(callback).not.toHaveBeenCalled();
       });
 
       it("should set #subscribed to true", function() {
         channel.cancelSubscription();
-        channel.handleEvent("pusher_internal:subscription_succeeded", "123")
+        channel.handleEvent("pusher_internal:subscription_succeeded", "123");
         expect(channel.subscribed).toEqual(true);
       });
 
@@ -219,39 +222,40 @@ describe("EncryptedChannel", function() {
     });
 
     describe("on other events", function() {
-      beforeEach(() => {
+      beforeEach(function() {
         // in order to decrypt encrypted events, we need to get a shared secret
         // from the authorizer.
         let authorizer = Mocks.getAuthorizer();
-        let callback = () => {}
+        let callback = function() {};
         factorySpy = spyOn(Factory, "createAuthorizer").andReturn(authorizer);
         channel.authorize("1.23", callback);
         authorizer._callback(false, {
           shared_secret: secretBase64,
           foo: "bar"
         });
-        let randomBytesSpy = spyOn(tweetNacl, 'randomBytes').andReturn(nonceBytes)
+        let randomBytesSpy = spyOn(tweetNacl, "randomBytes").andReturn(
+          nonceBytes
+        );
       });
       it("should decrypt the event payload and emit the event", function() {
-        let payload = {test: "payload"};
+        let payload = { test: "payload" };
         let encryptedPayload = {
           nonce: nonceBase64,
           ciphertext: testEncrypt(payload)
         };
         let boundCallback = jasmine.createSpy("boundCallback");
         channel.bind("something", boundCallback);
-        channel.handleEvent("something", encryptedPayload)
+        channel.handleEvent("something", encryptedPayload);
         expect(boundCallback).toHaveBeenCalledWith(payload);
       });
       it("should throw an error if the data payload is not encrypted", function() {
-        let payload = {test: "payload"};
+        let payload = { test: "payload" };
         let boundCallback = jasmine.createSpy("boundCallback");
         channel.bind("something", boundCallback);
-        expect(() => {
-          channel.handleEvent("something", payload)
+        expect(function() {
+          channel.handleEvent("something", payload);
         }).toThrow();
       });
     });
   });
 });
-
