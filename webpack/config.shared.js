8c4db8780449e2f7f3b648b7e7783541b6fc92ea
James Lees <james.lees@pusher.com>
2019-03-08 16:48:18 +0000

    Update deps and build

--- webpack/config.shared.js
+++ webpack/config.shared.js
@@ -2,46 +2,33 @@
 
 var webpack = require('webpack');
 var fs = require('fs');
-var StringReplacePlugin = require("string-replace-webpack-plugin");
 var Config = require('./hosting_config');
 
-var lookup = {
-  "<VERSION>": Config.version,
-  "<CDN_HTTP>": Config.cdn_http,
-  "<CDN_HTTPS>": Config.cdn_https,
-  "<DEPENDENCY_SUFFIX>": Config.dependency_suffix
-};
-
-var replacements = [];
-
-for (let i of Object.keys(lookup)) {
-  replacements.push({
-    pattern: i,
-    replacement: function() {
-      return lookup[i];
-    }
-  });
-}
-
+var mode = process.env.WEBPACK_MODE || 'production';
 module.exports = {
+  mode: mode,
+  devtool: false,
   entry: {
     pusher: "./src/core/index",
   },
   resolve: {
-    extensions: ['', '.webpack.js', '.web.js', '.ts', '.js']
+    extensions: ['.webpack.js', '.web.js', '.ts', '.js']
   },
   module: {
-    loaders: [
+    rules: [
       { test: /\.ts$/, loader: 'ts-loader' },
-      {
-        test: /.*/,
-        loader: StringReplacePlugin.replace({replacements: replacements})
-      },
-      { test : /\.js$/, loader: 'es3ify-loader'}
     ]
   },
   plugins: [
-    new webpack.BannerPlugin(fs.readFileSync('./src/core/pusher-licence.js', 'utf8').replace("<VERSION>", Config.version), {raw: true}),
-    new StringReplacePlugin()
+    new webpack.BannerPlugin({
+      banner: fs.readFileSync('./src/core/pusher-licence.js', 'utf8').replace("<VERSION>", Config.version),
+      raw: true
+    }),
+    new webpack.DefinePlugin({
+      "<VERSION>": Config.version,
+      "<CDN_HTTP>": Config.cdn_http,
+      "<CDN_HTTPS>": Config.cdn_https,
+      "<DEPENDENCY_SUFFIX>": Config.dependency_suffix
+    })
   ]
 }
