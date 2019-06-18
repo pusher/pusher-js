782ad3a967f025a89f87645b03b05ef8333cf849
James Lees <james.lees@pusher.com>
2019-06-18 11:03:46 -0400

    Update webpack, typescript and ts-loader
    - update dep versions
    - webpack config changes to reflect the new webpack API
    - replace string replace plugin with the webpack equivalent
    - replace the minified config with webpack mode, use dev mode for node
    - remove the unnecessary es3ify-loader
    - add a package-lock.json
    
    squash

--- webpack/config.web.js
+++ webpack/config.web.js
@@ -6,21 +6,26 @@ var objectAssign = require('object-assign-deep');
 /*
   Upon importing the 'runtime' module, this web build is made to look at
   src/runtimes/web/runtime.ts by the below webpack resolution config.
-  This is achieved by adding 'src/runtimes/web' to the resolve.modulesDirectories array
+  This is achieved by adding 'src/runtimes/web' to the resolve.modules array
 
   -- CONVENIENCE --
   We also add 'src/runtimes' to the list for convenient referencing of 'isomorphic/' implementations.
   We also add 'src/' so that the runtimes/web folder can conveniently import 'core/' modules.
 */
+
+var filename = process.env.MODE === "development" ?
+  "pusher.js":
+  "pusher.min.js";
+
 var config = objectAssign(require('./config.shared'),{
   output: {
     library: "Pusher",
     path: path.join(__dirname, "../dist/web"),
-    filename: "pusher.js",
+    filename: filename,
     libraryTarget: "umd"
   },
   resolve: {
-    modulesDirectories: ['src/', 'src/runtimes/web', 'src/runtimes', 'node_modules']
+    modules: ['src/', 'src/runtimes/web', 'src/runtimes', 'node_modules']
   },
   plugins: [
     new webpack.DefinePlugin({
