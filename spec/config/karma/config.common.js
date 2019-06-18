41947ed33878e520375e005f55029de86b09ba22
James Lees <james.lees@pusher.com>
2019-06-18 15:23:34 -0400

    update test configs
    try to make the test configs a little more consistent/concise
    
    - use verbose reporter again
    - use the webpack shared config as the basis for the karma webpack
    config and remove unnecessary modules
    - expose a MINIMAL_INTEGRATION_TESTS env var
    - build node configs the same as elsewhere

--- spec/config/karma/config.common.js
+++ spec/config/karma/config.common.js
@@ -1,33 +1,29 @@
+var webpackConfig = require('../../../webpack/config.shared');
+var objectAssign = require('object-assign-deep');
+
 module.exports = {
   basePath: '../../../',
   frameworks: ["jasmine"],
 
-  reporters: ['coverage', 'dots'],
+  reporters: ['coverage', 'verbose'],
 
   coverageReporter: {
     type : 'html',
     dir : 'coverage/'
   },
 
-  preprocessors: {
-    '**/spec/javascripts/node_modules/**/*.ts': ['webpack']
-  },
-
-  webpack: {
+  webpack: objectAssign({}, webpackConfig, {
+    mode: 'development',
     resolve: {
       modules: [
-        'node_modules',
-        'web_modules',
-        'src',
         'src/runtimes/web',
-        'src/runtimes',
         'spec/javascripts/helpers'
       ]
     },
     externals: {
       testenv: "'web'"
     }
-  },
+  }),
   port: 9876,
   runnerPort: 9100,
   colors: true,
