40a19cc5f4b4d3ac3206f4bfb38f585132f8a2b1
James Lees <james.lees@pusher.com>
2019-07-18 11:10:23 +0100

    only run browserstack with full

--- spec/config/karma/config.ci.js
+++ spec/config/karma/config.ci.js
@@ -1,37 +1,43 @@
-var config = {
-  browserStack: {
-    startTunnel: true,
-    timeout: 1800
-  },
-  customLaunchers: {
-    bs_chrome_74: {
-      base: 'BrowserStack',
-      os_version: "Mojave",
-      browser: "Chrome",
-      browser_version: "74.0",
-      os: "OS X"
-    },
-    bs_firefox_66: {
-      base: 'BrowserStack',
-      os_version: "Mojave",
-      browser: "Firefox",
-      browser_version: "66.0",
-      os: "OS X"
-    },
-    bs_safari_12: {
-      base: 'BrowserStack',
-      os_version: "Mojave",
-      browser: "Safari",
-      browser_version: "12",
-      os: "OS X"
-    }
-  }
-};
-
+var config;
 if (process.env.CI === 'full') {
+  config = getBrowserStackConfig();
   config.browsers = Object.keys(config.customLaunchers);
 } else {
-  config.browsers = ['bs_chrome_74'];
+  config = {
+    browsers: ['ChromeHeadless']
+  };
+}
+
+function getBrowserStackConfig() {
+  return {
+    browserStack: {
+      startTunnel: true,
+      timeout: 1800
+    },
+    customLaunchers: {
+      bs_chrome_74: {
+        base: 'BrowserStack',
+        os_version: "Mojave",
+        browser: "Chrome",
+        browser_version: "74.0",
+        os: "OS X"
+      },
+      bs_firefox_66: {
+        base: 'BrowserStack',
+        os_version: "Mojave",
+        browser: "Firefox",
+        browser_version: "66.0",
+        os: "OS X"
+      },
+      bs_safari_12: {
+        base: 'BrowserStack',
+        os_version: "Mojave",
+        browser: "Safari",
+        browser_version: "12",
+        os: "OS X"
+      }
+    }
+  };
 }
 
 module.exports = config;
