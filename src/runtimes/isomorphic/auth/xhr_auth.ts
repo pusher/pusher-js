c9933429062e4bba1f1bc56fc07d1d73e31598dc
James Lees <james.lees@pusher.com>
2019-09-24 15:49:00 +0100

    Fix the error message when authorization fails

--- src/runtimes/isomorphic/auth/xhr_auth.ts
+++ src/runtimes/isomorphic/auth/xhr_auth.ts
@@ -37,7 +37,8 @@ var ajax : AuthTransport = function(context : AbstractRuntime, socketId, callbac
       } else {
         var suffix = UrlStore.buildLogSuffix("authenticationEndpoint");
         Logger.warn(
-          `Couldn't retrieve authentication info. ${xhr.status}` +
+          'Unable to retrieve auth string from auth endpoint - ' +
+          `received status ${xhr.status} from ${self.options.authEndpoint}. ` +
           `Clients must be authenticated to join private or presence channels. ${suffix}`
         );
         callback(true, xhr.status);
