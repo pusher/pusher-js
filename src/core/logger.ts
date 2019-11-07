cac7255980a74977bbe40365fc161579deeb945a
Will Sewell <me@willsewell.com>
2019-11-07 18:07:50 +0000

    DRY up the Logger

--- src/core/logger.ts
+++ src/core/logger.ts
@@ -1,42 +1,46 @@
 import { stringify } from './utils/collections';
 import Pusher from './pusher';
 
+function globalLog(message) {
+  if (global.console && global.console.log) {
+    global.console.log(message);
+  }
+}
+
+function globalLogWarn(message) {
+  if (global.console && global.console.warn) {
+    global.console.warn(message);
+  } else {
+    globalLog(message);
+  }
+}
+
+function globalLogError(message) {
+  if (global.console && global.console.error) {
+    global.console.error(message);
+  } else {
+    globalLogWarn(message);
+  }
+}
+
+function log(defaultLoggingFunction, ...args: any[]) {
+  var message = stringify.apply(this, arguments);
+  if (Pusher.log) {
+    Pusher.log(message);
+  } else if (Pusher.logToConsole) {
+    defaultLoggingFunction(message);
+  }
+}
+
 const Logger = {
   debug(...args: any[]) {
-    var message = stringify.apply(this, arguments);
-    if (Pusher.log) {
-      Pusher.log(message);
-    } else if (Pusher.logToConsole && global.console) {
-      if (global.console.log) {
-        global.console.log(message);
-      }
-    }
+    log(globalLog, args);
   },
   warn(...args: any[]) {
-    var message = stringify.apply(this, arguments);
-    if (Pusher.log) {
-      Pusher.log(message);
-    } else if (Pusher.logToConsole && global.console) {
-      if (global.console.warn) {
-        global.console.warn(message);
-      } else if (global.console.log) {
-        global.console.log(message);
-      }
-    }
+    log(globalLogWarn, args);
   },
   error(...args: any[]) {
-    var message = stringify.apply(this, arguments);
-    if (Pusher.log) {
-      Pusher.log(message);
-    } else if (Pusher.logToConsole && global.console) {
-      if (global.console.error) {
-        global.console.error(message);
-      } else if (global.console.warn) {
-        global.console.warn(message);
-      } else if (global.console.log) {
-        global.console.log(message);
-      }
-    }
+    log(globalLogError, args);
   }
 };
 
