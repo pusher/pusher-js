7f5e8cf906485e7783ab01eba646d2e4f41b7954
Will Sewell <me@willsewell.com>
2019-11-14 10:29:30 +0000

    Turn Logger into a class in order to make method private

--- src/core/logger.ts
+++ src/core/logger.ts
@@ -1,50 +1,53 @@
 import { stringify } from './utils/collections';
 import Pusher from './pusher';
 
-function globalLog(message: string) {
-  if (global.console && global.console.log) {
-    global.console.log(message);
-  }
-}
-
-function globalLogWarn(message: string) {
-  if (global.console && global.console.warn) {
-    global.console.warn(message);
-  } else {
-    globalLog(message);
-  }
-}
-
-function globalLogError(message: string) {
-  if (global.console && global.console.error) {
-    global.console.error(message);
-  } else {
-    globalLogWarn(message);
-  }
-}
-
-function log(
-  defaultLoggingFunction: (message: string) => void,
-  ...args: any[]
-) {
-  var message = stringify.apply(this, arguments);
-  if (Pusher.log) {
-    Pusher.log(message);
-  } else if (Pusher.logToConsole) {
-    defaultLoggingFunction(message);
-  }
-}
-
-const Logger = {
+class Logger {
   debug(...args: any[]) {
-    log(globalLog, args);
-  },
-  warn(...args: any[]) {
-    log(globalLogWarn, args);
-  },
-  error(...args: any[]) {
-    log(globalLogError, args);
+    this.log(this.globalLog, args);
   }
-};
 
-export default Logger;
+  warn(...args: any[]) {
+    this.log(this.globalLogWarn, args);
+  }
+
+  error(...args: any[]) {
+    this.log(this.globalLogError, args);
+  }
+
+  private globalLog = (message: string) => {
+    if (global.console && global.console.log) {
+      global.console.log(message);
+    }
+  };
+
+  private globalLogWarn(message: string) {
+    if (global.console && global.console.warn) {
+      global.console.warn(message);
+    } else {
+      this.globalLog(message);
+    }
+  }
+
+  private globalLogError(message: string) {
+    if (global.console && global.console.error) {
+      global.console.error(message);
+    } else {
+      this.globalLogWarn(message);
+    }
+  }
+
+  private log(
+    defaultLoggingFunction: (message: string) => void,
+    ...args: any[]
+  ) {
+    var message = stringify.apply(this, arguments);
+    if (Pusher.log) {
+      Pusher.log(message);
+    } else if (Pusher.logToConsole) {
+      const log = defaultLoggingFunction.bind(this);
+      log(message);
+    }
+  }
+}
+
+export default new Logger();
