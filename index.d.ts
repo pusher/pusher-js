497ee048480b21a9a1a9dc26147ee013a9a228b9
James Lees <james.lees@pusher.com>
2020-02-11 10:09:33 +0000

    prettier on declarations

--- index.d.ts
+++ index.d.ts
@@ -3,14 +3,14 @@ export {
   Authorizer,
   AuthOptions,
   AuthorizerGenerator,
-  AuthorizerCallback,
+  AuthorizerCallback
 } from './types/src/core/auth/options';
-export { Options } from './types/src/core/options'
+export { Options } from './types/src/core/options';
 
-export {default as Channel} from './types/src/core/channels/channel';
-export {default as PresenceChannel} from './types/src/core/channels/presence_channel';
-export {default as Members} from './types/src/core/channels/members';
-export {default as  Runtime}  from './types/src/runtimes/interface'
-export {default as ConnectionManager}  from './types/src/core/connection/connection_manager'
+export { default as Channel } from './types/src/core/channels/channel';
+export { default as PresenceChannel } from './types/src/core/channels/presence_channel';
+export { default as Members } from './types/src/core/channels/members';
+export { default as Runtime } from './types/src/runtimes/interface';
+export { default as ConnectionManager } from './types/src/core/connection/connection_manager';
 
-export {default} from './types/src/core/pusher'
+export { default } from './types/src/core/pusher';
