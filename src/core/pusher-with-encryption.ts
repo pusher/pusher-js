6e5bb2f2fc3ef1803dbd68e2e2f9980751ca299e
James Lees <james.lees@pusher.com>
2020-04-23 13:14:29 +0100

    Add a pusher-with-encrytion subclass
    This is a thin wrapper around the normal Pusher class which just injects
    the tweetnacl-js implementation as the nacl option

---
+++ src/core/pusher-with-encryption.ts
@@ -0,0 +1,10 @@
+import Pusher from './pusher'
+import { Options } from './options';
+import * as nacl from 'tweetnacl';
+
+export default class PusherWithEncryption extends Pusher {
+  constructor(app_key: string, options?: Options) {
+    options.nacl = nacl;
+    super(app_key, options)
+  }
+}
