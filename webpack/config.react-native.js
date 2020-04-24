e30893113175986be62d79bb23d55c75d2819de8
James Lees <james.lees@pusher.com>
2020-04-24 13:14:20 +0100

    update react-native build for encrypted channels

--- webpack/config.react-native.js
+++ webpack/config.react-native.js
@@ -5,15 +5,18 @@ var version = require('../package').version;
 var objectAssign = require('object-assign-deep');
 var configShared = require('./config.shared');
 var webpack = require('webpack');
+var buffer = require('buffer');
 
 module.exports = objectAssign({}, configShared, {
+  entry: {
+    pusher: './src/core/index-with-encryption',
+  },
   output: {
     library: 'Pusher',
     libraryTarget: 'commonjs2',
     path: path.join(__dirname, '../dist/react-native'),
     filename: 'pusher.js'
   },
-  target: 'node',
   externals: {
     // our Reachability implementation needs to reference @react-native-community/netinfo.
     '@react-native-community/netinfo': '@react-native-community/netinfo'
@@ -24,6 +27,9 @@ module.exports = objectAssign({}, configShared, {
   plugins: [
     new webpack.DefinePlugin({
       RUNTIME: JSON.stringify('react-native')
+    }),
+    new webpack.ProvidePlugin({
+      buffer: 'buffer'
     })
   ]
 });
