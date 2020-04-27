139bcc32c31ca961b8858bc829a9141bac92ff95
James Lees <james.lees@pusher.com>
2020-04-27 11:55:53 +0100

    pass global config to subclass (#449)
    2 parameters can be set by the caller
    
    ```
    import Pusher from 'pusher-js;
    Pusher.logToConsole;
    ```
    
    etc
    
    We need to pass these properties on to the subclass

--- src/core/pusher-with-encryption.ts
+++ src/core/pusher-with-encryption.ts
@@ -4,6 +4,9 @@ import * as nacl from 'tweetnacl';
 
 export default class PusherWithEncryption extends Pusher {
   constructor(app_key: string, options?: Options) {
+    Pusher.logToConsole = PusherWithEncryption.logToConsole;
+    Pusher.log = PusherWithEncryption.log;
+
     options = options || {};
     options.nacl = nacl;
     super(app_key, options);
