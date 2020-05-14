9daa2ee8afcd1ad70e31288652fd24e2ec49c241
James Lees <james.lees@pusher.com>
2020-05-14 15:22:40 +0100

    add types for with-encryption (#461)

---
+++ with-encryption/index.d.ts
@@ -0,0 +1,16 @@
+export {
+  AuthInfo,
+  Authorizer,
+  AuthOptions,
+  AuthorizerGenerator,
+  AuthorizerCallback
+} from '../types/src/core/auth/options';
+export { Options } from '../types/src/core/options';
+
+export { default as Channel } from '../types/src/core/channels/channel';
+export { default as PresenceChannel } from '../types/src/core/channels/presence_channel';
+export { default as Members } from '../types/src/core/channels/members';
+export { default as Runtime } from '../types/src/runtimes/interface';
+export { default as ConnectionManager } from '../types/src/core/connection/connection_manager';
+
+export { default } from '../types/src/core/pusher';
