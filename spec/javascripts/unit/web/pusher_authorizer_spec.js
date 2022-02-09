3b99f26a3635abcc70c22d522f7cd35179892ba6
Meena Alfons <meena.alfons@messagebird.com>
2022-02-09 13:26:59 +0100

    more tests

--- spec/javascripts/unit/web/pusher_authorizer_spec.js
+++ spec/javascripts/unit/web/pusher_authorizer_spec.js
@@ -1,4 +1,4 @@
-var Authorizer = require('core/auth/pusher_authorizer').default;
+var ChannelAuthorizer = require('core/auth/channel_authorizer').default;
 var Logger = require('core/logger');
 var Mocks = require('mocks');
 var Util = require('core/util').default;
@@ -6,17 +6,13 @@ var Factory = require('core/utils/factory').default;
 var Logger = require('core/logger').default;
 var Runtime = require('runtime').default;
 
-describe("JSONP Authorizer", function() {
-  it("should raise a warning if headers are passed", function() {
-    var headers = { "foo": "bar", "n": 42 };
-    var authorizer = new Authorizer(
-      { name: "chan" },
-      { authTransport: "jsonp",
-        auth: {
-          headers: headers
-        }
-      }
-    );
+describe('JSONP Authorizer', function() {
+  it('should raise a warning if headers are passed', function() {
+    var headers = { foo: 'bar', n: 42 };
+    var channelAuthorizer = ChannelAuthorizer({
+      transport: 'jsonp',
+      headers: headers
+    })
 
     var document = Mocks.getDocument();
     var script = Mocks.getDocumentElement();
@@ -25,13 +21,16 @@ describe("JSONP Authorizer", function() {
     document.createElement.and.returnValue(script);
     document.getElementsByTagName.and.returnValue([]);
     document.documentElement = documentElement;
-    spyOn(Runtime, "getDocument").and.returnValue(document);
+    spyOn(Runtime, 'getDocument').and.returnValue(document);
 
-    spyOn(Logger, "warn");
-    authorizer.authorize("1.23", function() {});
+    spyOn(Logger, 'warn');
+    channelAuthorizer({
+      socketId: '1.23',
+      channelName: 'chan',
+    }, function() {})
 
     expect(Logger.warn).toHaveBeenCalledWith(
-      "To send headers with the auth request, you must use AJAX, rather than JSONP."
+      'To send headers with the auth request, you must use AJAX, rather than JSONP.'
     );
   });
 });
--- spec/javascripts/unit/worker/pusher_authorizer_spec.js
+++ spec/javascripts/unit/worker/pusher_authorizer_spec.js
@@ -1,111 +1,113 @@
-var Authorizer = require('core/auth/pusher_authorizer').default;
 var fetchAuth = require('worker/auth/fetch_auth').default;
 var Runtime = require('runtime').default;
 var fetchMock = require('fetch-mock');
+var ChannelAuthorizer = require('core/auth/channel_authorizer').default;
 
 var endpoint = 'http://example.org/pusher/auth';
 
-describe("Fetch Authorizer", function(){
-
-  beforeEach(function(){
-    Authorizer.authorizers = {ajax: fetchAuth}
+describe('Fetch Authorizer', function() {
+  beforeEach(function() {
+    print(Runtime.getAuthorizers())
+    // Authorizer.authorizers = { ajax: fetchAuth };
   });
 
-  afterEach(function(){
+  afterEach(function() {
     fetchMock.restore();
   });
 
-  it("should pass headers in the request", function(){
-    fetchMock
-      .mock(endpoint, {body: {hello: "world"}});
+  it('should pass headers in the request', function() {
+    fetchMock.mock(endpoint, { body: { hello: 'world' } });
 
-    var headers = { "foo": "bar", "n": 42 };
-    var authorizer = new Authorizer(
-      { name: "chan" },
-      { authTransport: "ajax",
-        authEndpoint: endpoint,
-        auth: {
-          headers: headers
-        }
-      }
-    );
+    var headers = { foo: 'bar', n: 42 };
+    var channelAuthorizer = ChannelAuthorizer({
+      transport: 'ajax',
+      endpoint: endpoint,
+      headers: headers
+    });
 
-    authorizer.authorize("1.23", function() {});
+    channelAuthorizer({
+      sockerId: '1.23',
+      channelName: 'chan'
+    }, function() {});
 
     var lastCall = fetchMock.lastCall(endpoint)[0];
     var sentHeaders = lastCall.headers;
-    expect(sentHeaders.get("Content-Type")).toEqual("application/x-www-form-urlencoded");
-    expect(sentHeaders.get("foo")).toEqual("bar");
-    expect(sentHeaders.get("n")).toEqual('42');
-  });
-
-  it("should pass params in the query string", function(){
-    fetchMock
-      .mock(endpoint, {body: {hello: "world"}});
-
-    var params = { "a": 1, "b": 2 };
-    var authorizer = new Authorizer(
-      { name: "chan" },
-      { authTransport: "ajax",
-        authEndpoint: endpoint,
-        auth: {
-          params: params
-        }
-      }
+    expect(sentHeaders.get('Content-Type')).toEqual(
+      'application/x-www-form-urlencoded'
     );
-    authorizer.authorize("1.23", function() {}).then(function(){
-      var lastCall = fetchMock.lastCall(endpoint)[0];
-      console.log(lastCall);
-      expect(lastCall.body).toEqual("socket_id=1.23&channel_name=chan&a=1&b=2");
-    });
+    expect(sentHeaders.get('foo')).toEqual('bar');
+    expect(sentHeaders.get('n')).toEqual('42');
   });
 
-  it("should call back with the auth result on success", function(){
-    var data = { foo: "bar", number: 1};
+  it('should pass params in the query string', function() {
+    fetchMock.mock(endpoint, { body: { hello: 'world' } });
+
+    var params = { a: 1, b: 2 };
+    var channelAuthorizer = ChannelAuthorizer({
+      transport: 'ajax',
+      endpoint: endpoint,
+      params: params
+    });
+
+    channelAuthorizer({
+      sockerId: '1.23',
+      channelName: 'chan'
+    }, function() {});
+
+    var lastCall = fetchMock.lastCall(endpoint)[0];
+    console.log(lastCall);
+    expect(lastCall.body).toEqual(
+      'socket_id=1.23&channel_name=chan&a=1&b=2'
+    );
+  });
+
+  it('should call back with the auth result on success', function() {
+    var data = { foo: 'bar', number: 1 };
     var dataJSON = JSON.stringify(data);
 
     fetchMock.mock(endpoint, {
       body: dataJSON
-    })
-
-    var authorizer = new Authorizer(
-      { name: "chan" },
-      {
-        authTransport: "ajax",
-        authEndpoint: endpoint
-      }
-    );
-    var callback = jasmine.createSpy("callback");
-    authorizer.authorize("1.23", callback).then(function(){
-      expect(callback.calls.count()).toEqual(1);
-      expect(callback).toHaveBeenCalledWith(false, data);
     });
+
+    var channelAuthorizer = ChannelAuthorizer({
+      transport: 'ajax',
+      endpoint: endpoint,
+    });
+
+    var callback = jasmine.createSpy('callback');
+    channelAuthorizer({
+      sockerId: '1.23',
+      channelName: 'chan'
+    }, callback);
+    
+    expect(callback.calls.count()).toEqual(1);
+    expect(callback).toHaveBeenCalledWith(false, data);
   });
 
-  it("should call back with an error if JSON is invalid", function(){
-    var authorizer = new Authorizer(
-      { name: "chan" },
-      {
-        authTransport: "ajax",
-        authEndpoint: endpoint
-      }
-    );
+  it('should call back with an error if JSON is invalid', function() {
+    var channelAuthorizer = ChannelAuthorizer({
+      transport: 'ajax',
+      endpoint: endpoint,
+    });
+
 
     var invalidJSON = 'INVALID { "something": "something"}';
     fetchMock.mock(endpoint, {
       body: invalidJSON
-    })
-
-    var callback = jasmine.createSpy("callback");
-
-    authorizer.authorize("1.23", callback).then(function(){
-      expect(callback.calls.count()).toEqual(1);
-      expect(callback).toHaveBeenCalledWith(
-        true,
-        "JSON returned from auth endpoint was invalid, yet status code was 200. " +
-          "Data was: " +
-          invalidJSON
-      );
     });
+
+    var callback = jasmine.createSpy('callback');
+    channelAuthorizer({
+      sockerId: '1.23',
+      channelName: 'chan'
+    }, callback);
+    
+    expect(callback.calls.count()).toEqual(1);
+    expect(callback).toHaveBeenCalledWith(
+      true,
+      'JSON returned from auth endpoint was invalid, yet status code was 200. ' +
+        'Data was: ' +
+        invalidJSON
+    );
   });
 });
