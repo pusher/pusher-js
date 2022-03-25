18a64e473c51d840c2b7f335389331f55d4447b8
Gonçalo Martins <gonga.martins@gmail.com>
2022-03-25 13:42:24 +0000

    Import randomInt function only instead of importing whole crypto module

--- src/runtimes/node/runtime.ts
+++ src/runtimes/node/runtime.ts
@@ -6,7 +6,7 @@ import { Network } from './net_info';
 import xhrAuth from 'isomorphic/auth/xhr_auth';
 import { AuthTransports } from 'core/auth/auth_transports';
 import xhrTimeline from 'isomorphic/timeline/xhr_timeline';
-import * as crypto from 'crypto';
+import { randomInt } from 'crypto';
 
 // Very verbose but until unavoidable until
 // TypeScript 2.1, when spread attributes will be
@@ -61,7 +61,7 @@ const NodeJS: Runtime = {
   },
 
   randomInt(max: number): number {
-    return crypto.randomInt(max);
+    return randomInt(max);
   }
 };
 
--- src/runtimes/react-native/runtime.ts
+++ src/runtimes/react-native/runtime.ts
@@ -4,7 +4,7 @@ import { Network } from './net_info';
 import xhrAuth from 'isomorphic/auth/xhr_auth';
 import { AuthTransports } from 'core/auth/auth_transports';
 import xhrTimeline from 'isomorphic/timeline/xhr_timeline';
-import * as crypto from 'crypto';
+import { randomInt } from 'crypto';
 
 // Very verbose but until unavoidable until
 // TypeScript 2.1, when spread attributes will be
@@ -59,7 +59,7 @@ const ReactNative: Runtime = {
   },
 
   randomInt(max: number): number {
-    return crypto.randomInt(max);
+    return randomInt(max);
   }
 };
 
