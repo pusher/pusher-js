e88645f5850d611bcf2f57b0c67e6e3e952d1778
Meena Alfons <meena.alfons@messagebird.com>
2022-08-05 14:26:11 +0200

    Allow presence channel authorization to depend on user authentication

---
+++ src/core/utils/flat_promise.ts
@@ -0,0 +1,10 @@
+function flatPromise() {
+  let resolve, reject;
+  const promise = new Promise((res, rej) => {
+    resolve = res;
+    reject = rej;
+  });
+  return { promise, resolve, reject };
+}
+
+export default flatPromise;
