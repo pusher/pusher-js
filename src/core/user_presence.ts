e155b15bbf54e6c9ada1aa9dfede84c04bea8770
fbenevides <me@benevides.pro>
2022-09-07 17:14:26 +0200

    Create UserPresence face to handle user presence events

---
+++ src/core/user_presence.ts
@@ -0,0 +1,41 @@
+import Pusher from './pusher';
+
+export default class UserPresenceFacade {
+  _pusher: Pusher
+  _eventHandlers: Map<string, Function>
+
+  public constructor(pusher: Pusher) {
+    this._pusher = pusher;
+    this._eventHandlers = new Map<string, Function>();
+    this.bindUserPresenceEvents();
+  }
+
+  bind(events: Array<string>, callback: Function) {
+    events.forEach(eventName => this._eventHandlers[eventName] = callback)
+    return this;
+  }
+
+  private bindUserPresenceEvents() {
+    this._pusher.connection.bind('message', event => {
+      var eventName = event.event;
+      if (eventName === 'pusher_internal:user_presence') {
+        this.handleUserPresenceEvent(event);
+      }
+    });
+  }
+
+  private handleUserPresenceEvent(event) {
+    event.data.events.forEach(userPresenceEvent => {
+      const eventHandler: Function = this._eventHandlers[userPresenceEvent.action];
+      if (eventHandler) {
+        userPresenceEvent.users
+          .map(userId => this.buildUserPresenceEvent(userId, userPresenceEvent))
+          .forEach(finalEvent => eventHandler.call(global, finalEvent));
+      }
+    });
+  }
+
+  private buildUserPresenceEvent(userId: string, event: any): object {
+    return { action: event.action, user_id: userId, channel_name: event.channel_name }
+  }
+}
