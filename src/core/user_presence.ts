a209225ce1626fd97bf6f0fb56635a329a527ea2
fbenevides <me@benevides.pro>
2022-09-07 18:24:08 +0200

    Code format

--- src/core/user_presence.ts
+++ src/core/user_presence.ts
@@ -3,24 +3,28 @@ import Pusher from './pusher';
 import EventsDispatcher from './events/dispatcher';
 
 export default class UserPresenceFacade extends EventsDispatcher {
-  private pusher: Pusher
-  private syntaxSugars: Map<string, Array<string>> 
+  private pusher: Pusher;
+  private syntaxSugars: Map<string, Array<string>>;
 
   public constructor(pusher: Pusher) {
     super(function(eventName, data) {
       Logger.debug(`No callbacks on user presence for ${eventName}`);
     });
-    
+
     this.pusher = pusher;
-    
+
     this.initializeSyntaxSugars();
     this.bindUserPresenceEvents();
   }
 
-  bind(events: string | Array<string>, callback: Function, context?: any): this {
+  bind(
+    events: string | Array<string>,
+    callback: Function,
+    context?: any
+  ): this {
     let userPresenceEvents = [];
-    
-    if (typeof(events) === 'string') {
+
+    if (typeof events === 'string') {
       const syntaxSugarEvents = this.syntaxSugars.get(events);
       if (syntaxSugarEvents !== undefined) {
         userPresenceEvents = syntaxSugarEvents;
@@ -31,14 +35,19 @@ export default class UserPresenceFacade extends EventsDispatcher {
       userPresenceEvents = events;
     }
 
-    userPresenceEvents.forEach(eventName => super.bind(eventName, callback, context))
+    userPresenceEvents.forEach(eventName =>
+      super.bind(eventName, callback, context)
+    );
     return this;
   }
 
   private initializeSyntaxSugars() {
     this.syntaxSugars = new Map();
     this.syntaxSugars.set('online-status', ['online', 'offline']);
-    this.syntaxSugars.set('channel-subscription', ['subscribed', 'unsubscribed']);
+    this.syntaxSugars.set('channel-subscription', [
+      'subscribed',
+      'unsubscribed'
+    ]);
   }
 
   private bindUserPresenceEvents() {
@@ -59,6 +68,10 @@ export default class UserPresenceFacade extends EventsDispatcher {
   }
 
   private buildUserPresenceEvent(userId: string, event: any): object {
-    return { action: event.action, user_id: userId, channel_name: event.channel_name }
+    return {
+      action: event.action,
+      user_id: userId,
+      channel_name: event.channel_name
+    };
   }
 }
