b917581ab8cc036c688c86932353b0ac27e8e4d2
fbenevides <me@benevides.pro>
2023-03-15 16:53:17 +0100

    Fix react native support

--- src/runtimes/react-native/runtime.ts
+++ src/runtimes/react-native/runtime.ts
@@ -4,7 +4,6 @@ import { Network } from './net_info';
 import xhrAuth from 'isomorphic/auth/xhr_auth';
 import { AuthTransports } from 'core/auth/auth_transports';
 import xhrTimeline from 'isomorphic/timeline/xhr_timeline';
-import { randomInt } from 'crypto';
 
 // Very verbose but until unavoidable until
 // TypeScript 2.1, when spread attributes will be
@@ -59,7 +58,7 @@ const ReactNative: Runtime = {
   },
 
   randomInt(max: number): number {
-    return randomInt(max);
+    return Math.floor(Math.random() * max);
   }
 };
 
--- webpack/config.react-native.js
+++ webpack/config.react-native.js
@@ -29,7 +29,7 @@ module.exports = merge({}, configShared, {
       RUNTIME: JSON.stringify('react-native')
     }),
     new webpack.ProvidePlugin({
-      buffer: 'buffer'
+      Buffer: ['buffer', 'Buffer']
     })
   ]
 });
