93925cee7c3952dc689aede8837ad7d924f8ad1f
fbenevides <me@benevides.pro>
2023-06-12 12:03:11 +0200

    Accept 201 status codes

--- src/runtimes/worker/auth/fetch_auth.ts
+++ src/runtimes/worker/auth/fetch_auth.ts
@@ -39,7 +39,7 @@ var fetchAuth: AuthTransport = function(
   return fetch(request)
     .then(response => {
       let { status } = response;
-      if (status === 200) {
+      if (status === 200 || status === 201) {
         // manually parse the json so we can provide a more helpful error in
         // failure case
         return response.text();
